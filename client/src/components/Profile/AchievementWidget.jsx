fc57b241e5ebfd5540aa69e1de61547555f82c37
Duke Romkey <dukeromkey@gmail.com>
2022-11-30 22:28:25 -0600

    Addition of static content for profile components

---
+++ client/src/components/Profile/AchievementWidget.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './profilestyles.css';
+
+function AchievementWidget() {
+  return (
+    <div className="achievement_widget">
+      <h2>Recent Achievement/Your Rank</h2>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg" height="200" />
+    </div>
+  )
+}
+
+export default AchievementWidget;
