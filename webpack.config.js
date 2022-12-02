a05f111b6c68f838c95fef7a40444474c2e6d6ab
Nick Hays <nicholasahays@gmail.com>
2022-12-02 10:04:40 -0700

    Adds ts to webpack, have not changed entry point yet

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const webpack = require("webpack");
 
 module.exports = {
   entry: path.join(__dirname, "/client/src/index.jsx"),
+  output: {
+    path: path.join(__dirname, "/client/dist"),
+    filename: "bundle.js"
+  },
   module: {
     rules: [
       {
@@ -12,15 +16,16 @@ module.exports = {
         options: { presets: ['@babel/preset-react', '@babel/preset-env'] }
       },
       {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      }
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
       }
     ]
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.join(__dirname, "/client/dist"),
-    filename: "bundle.js"
-  },
+  resolve: { extensions: ["*", ".js", ".jsx", ".tsx", ".ts"] },
   mode: 'development'
 };
