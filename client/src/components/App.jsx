eeb6872b8c9c752403641b83c538d011051fd107
Duke Romkey <dukeromkey@gmail.com>
2022-12-02 15:57:22 -0600

    Tailwind build script path updated

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-// import '../styles/tailwind.css'
+import '../../dist/styles/styles.css'
 import Sidebar from './Sidebar/Sidebar.jsx';
 import Header from './Header.jsx';
 import Profile from './Profile/Profile.jsx';
 
 function App() {
   return (
-    <>
+    <div className="flex">
       <Sidebar />
-      <div className="bg-white">
+      <div>
         <Header />
         <Profile />
       </div>
-    </>
+    </div>
   )
 }
 
