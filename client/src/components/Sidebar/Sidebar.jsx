a42b3c7f1544326b4bcaaf922099b2167a0bd7d9
Duke Romkey <dukeromkey@gmail.com>
2022-12-03 21:23:51 -0600

    Added css and clickhandling to sidebar nav menu

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BabyCoinLogo from '../../../dist/assets/BabyCoin.png';
 
 const pages = ['Home', 'Market Watch', 'Leader Board', 'Achievements', 'Support'];
-const navItems = pages.map((page, index) => (
-  <button key={page + index} className="text-center w-11/12 py-3 px-2 hover:bg-neutral-700 hover:rounded-l-3xl hover:ml-6">{page}</button>
-));
 
 function Sidebar() {
+
+  const [activePage, setActivePage] = useState('Home');
+
+  function handleNavClick(e) {
+    e.preventDefault();
+    setActivePage(e.target.name);
+  }
+
+  const navItems = pages.map((page, index) => (
+    <button onClick={handleNavClick} name={page} key={page + index} className={`text-center w-11/12 py-3 px-2 hover:ml-6 ${activePage === page ? 'bg-neutral-700 rounded-l-3xl ml-6' : ''}`}>{page}</button>
+  ));
+
   return (
     <div className="w-1/5 border-r-2 border-zinc-800">
       <div className="h-full">
