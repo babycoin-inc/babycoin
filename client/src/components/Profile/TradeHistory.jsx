df4572cc8914d234ee7442c6cf6b564782ad9cfe
Duke Romkey <dukeromkey@gmail.com>
2022-12-04 00:19:32 -0600

    Modifying trade history div shape to tab structure

--- client/src/components/Profile/TradeHistory.jsx
+++ client/src/components/Profile/TradeHistory.jsx
@@ -41,30 +41,36 @@ const history = [
 
 function TradeHistory() {
   return (
-    <div className="bg-zinc-800 border-2 border-zinc-700 rounded">
-      <h2>Trade History</h2>
-      <table className="w-4/5 mx-auto border-collapse border-spacing-0 table-auto">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Amount</th>
-            <th>Price</th>
-            <th>Type</th>
-            <th>Balance</th>
-          </tr>
-        </thead>
-        <tbody>
-          {history.map((trade, index) => (
-            <tr key={trade.coin + index} className="border-b-2 border-black">
-              <td className="text-center">{trade.coin}<i className="block text-sm">{trade.symbol}</i></td>
-              <td className="text-center">{trade.amount}<i className="block text-sm">.{`${trade.amount} ${trade.symbol}`}</i></td>
-              <td className="text-center">{trade.purchase}</td>
-              <td className="text-center">{trade.order}</td>
-              <td className="text-center">{trade.balance}</td>
+    <div>
+      <div className="bg-zinc-800 rounded-t-xl py-3 px-5 w-fit">
+        <h2>Trade History</h2>
+      </div>
+      <div className="bg-zinc-800 rounded-b-xl rounded-tr-xl py-6">
+        <table className="w-full mx-auto border-collapse border-spacing-0 table-auto">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Amount</th>
+              <th>Price</th>
+              <th>Type</th>
+              <th>Balance</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {history.map((trade, index) => (
+              <tr key={trade.coin + index} className="border-b-2 border-black">
+                <td><img src="https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png" className="h-10 mx-auto" /></td>
+                <td className="text-center">{trade.coin}<i className="block text-sm">{trade.symbol}</i></td>
+                <td className="text-center">{trade.amount}<i className="block text-sm">.{`${trade.amount} ${trade.symbol}`}</i></td>
+                <td className="text-center">{trade.purchase}</td>
+                <td className="text-center">{trade.order}</td>
+                <td className="text-center">{trade.balance}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
