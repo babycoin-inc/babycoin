603413125c491c2df9a4ce8c2706672ae620490a
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-12-06 14:28:38 -0600

    Complete initial Header implementation

--- client/src/components/Achievements/Header.jsx
+++ client/src/components/Achievements/Header.jsx
@@ -3,9 +3,20 @@ import React from 'react';
 const Header = function () {
 
     return (
-        <div>
-            Header
+      <div className="">    
+        <div className="flex justify-center py-4">
+          <h1 className="text-4xl text-blue-400 font-bold">
+            500 Achievement Points
+          </h1>
         </div>
+        <div className="flex justify-center">
+            <select className="select bg-transparent rounded-md text-sm font-bold">
+                <option selected value="all">All</option>
+                <option value="complete">Complete</option>
+                <option value="incomplete">Incomplete</option>
+            </select>
+        </div>
+      </div>
     );
 };
 
