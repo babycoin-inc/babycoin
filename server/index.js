b4cf5ab8137aaa97cba9e0bd442b0f046cf32ce8
Duke Romkey <dukeromkey@gmail.com>
2022-12-05 21:28:58 -0600

    Added middleware

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const port = 3000
 
 app.use(express.static(__dirname + '/../client/dist'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
