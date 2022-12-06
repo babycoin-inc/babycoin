2f6fab6af5466537497b036aafd40dc665b1cb0c
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-12-06 14:28:23 -0600

    Complete initial Footer implementation

--- client/src/components/Achievements/Footer.jsx
+++ client/src/components/Achievements/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = function () {
         <footer>
             <div className="flex justify-center">
                 <footer className="text-center text-xs">
-                    <span>Icons by website</span>
+                    <span>• Icons by <a  className="text-cyan-500" href="https://icons8.com/" target="_blank">icons8</a> •</span>
                 </footer>
             </div>
         </footer>
