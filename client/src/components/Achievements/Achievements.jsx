50652bf13b09e64db844acb036e83937b2726415
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-12-05 21:26:02 -0600

    Initial commit for Achievements component

---
+++ client/src/components/Achievements/Achievements.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Achievements = function () {
+
+    return (
+        <footer>
+            <div className="flex justify-center">
+                <footer className="text-center text-[50px]">
+                    <span>Icons by website</span>
+                </footer>
+            </div>
+        </footer>
+    );
+};
+
+export default Achievements;
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from './Sidebar/Sidebar.jsx';
 import Header from './Header/Header.jsx';
 import Profile from './Profile/Profile.jsx';
+import Achievements from "./Achievements/Achievements.jsx";
 
 // YOU WILL CONDITIONALLY REPLACE THE PROFILE COMPONENT WITH TRADE, LEADER BOARD, ETC...
 
@@ -13,8 +14,11 @@ function App() {
         <div className="h-1/6">
           <Header />
         </div>
-        <div className="p-8 h-full bg-zinc-800">
+        {/* <div className="p-8 h-full bg-zinc-800">
           <Profile />
+        </div> */}
+        <div>
+          <Achievements />
         </div>
       </div>
     </div>
