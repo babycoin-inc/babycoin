f6038422e7fa2c0840ecec1f5a685a4cf6d58d63
Duke Romkey <dukeromkey@gmail.com>
2022-12-05 20:49:59 -0600

    Added mailto link as well as state change for nav link

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from './Sidebar/Sidebar.jsx';
 import Header from './Header/Header.jsx';
-import Profile from './Profile/Profile.jsx';
+import Home from './Profile/Home.jsx';
 
 // YOU WILL CONDITIONALLY REPLACE THE PROFILE COMPONENT WITH TRADE, LEADER BOARD, ETC...
 
 function App() {
+
+  const [activePage, setActivePage] = useState('Home');
+
+  function handleNavClick(e) {
+    e.preventDefault();
+    setActivePage(e.target.name);
+  }
+
+  let activeComponent;
+
+  if (activePage === 'Home') {
+    activeComponent = (<Home />);
+  } else if (activePage === 'Market Watch') {
+    activeComponent = (<h1>Insert Market Watch</h1>);
+  } else if (activePage === 'Leader Board') {
+    activeComponent = (<h1>Insert Leader Board</h1>);
+  } else if (activePage === 'Achievements') {
+    activeComponent = (<h1>Insert Achievements</h1>);
+  };
+
   return (
     <div className="flex m-0 p-0 max-w-screen-xl mx-auto text-neutral-100 bg-zinc-900 border-2 border-zinc-800">
-      <Sidebar />
+      <Sidebar handleNavClick={handleNavClick} activePage={activePage} />
       <div className="w-full h-full">
         <div className="h-1/6">
           <Header />
         </div>
         <div className="p-8 h-full bg-zinc-800">
-          <Profile />
+          {activeComponent}
         </div>
       </div>
     </div>
--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import BabyCoinLogo from '../../../dist/assets/BabyCoin.png';
 
-const pages = ['Home', 'Market Watch', 'Leader Board', 'Achievements', 'Support'];
+const pages = ['Home', 'Market Watch', 'Leader Board', 'Achievements'];
 
-function Sidebar() {
-
-  const [activePage, setActivePage] = useState('Home');
-
-  function handleNavClick(e) {
-    e.preventDefault();
-    setActivePage(e.target.name);
-  }
+function Sidebar({ handleNavClick, activePage }) {
 
   const navItems = pages.map((page, index) => (
     <button onClick={handleNavClick} name={page} key={page + index} className={`text-center w-11/12 py-3 px-2 hover:ml-6 ${activePage === page ? 'bg-zinc-800 rounded-l-3xl ml-6' : ''}`}>{page}</button>
@@ -24,6 +17,9 @@ function Sidebar() {
         </div>
         <nav className="flex flex-col items-center mt-10 w-full text-2xl">
           {navItems}
+          <a href="mailto:support@babycoin.com?subject=Support Request">
+            <button name="Support" className="text-center w-11/12 py-3 px-2 hover:ml-6">Support</button>
+          </a>
         </nav>
         <div className="mt-36">
           <h2 className="text-center">Watchlist Placeholder</h2>
