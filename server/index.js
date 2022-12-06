b465b54c6a0b150f43b06bcbcc7e0a5cab8287fd
Duke Romkey <dukeromkey@gmail.com>
2022-12-05 21:30:32 -0600

    Removed middleware to pass commit

--- server/index.js
+++ server/index.js
@@ -4,9 +4,6 @@ const port = 3000
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
