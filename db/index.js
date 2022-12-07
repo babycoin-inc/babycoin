2bd2d26722a811b918dc178af9c99c18aee3b772
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-12-06 22:28:55 -0600

    Initialize sql file

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: process.env.PGUSER,
-    // password: process.env.PGPASSWORD,
+    password: process.env.PGPASSWORD,
     database: process.env.PGDATABASE,
     host: process.env.PGHOST,
     port: process.env.PGPORT,
