62f40d682ab226c1ed8f2ec21f47ca045664b9a6
Nick Hays <nicholasahays@gmail.com>
2022-12-07 00:21:27 -0700

    Requires db index file so that the db connection test will run and adds missing semi-colons

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+require('../db/index.js');
+const app = express();
+const port = 3000;
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
