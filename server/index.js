5728ad44b9d0782b759dd458ecb93bc3d7916c8e
Nick Hays <nicholasahays@gmail.com>
2022-12-07 02:05:42 -0700

    Adds signup route and controller.

--- server/index.js
+++ server/index.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+/**Controllers */
+const { signup } = require('./controllers/Login/controllers.js');
+
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.post('/signup', signup);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
