5dbcd642787fb785a89fadd89ac5fda75f4e8671
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-12-07 21:35:01 -0600

    Add db initialization and read from sql file

--- db/index.js
+++ db/index.js
@@ -1,22 +1,54 @@
 require("dotenv").config();
-const { Pool } = require('pg');
+const { Pool, Client } = require('pg');
+const fs = require('fs');
 
 const pool = new Pool({
-    user: process.env.PGUSER,
+    user: process.env.USER,
     password: process.env.PGPASSWORD,
     database: process.env.PGDATABASE,
     host: process.env.PGHOST,
     port: process.env.PGPORT,
 });
 
+const sql = fs.readFileSync('db/init.sql', 'utf8');
+
 (async() => {
   try {
     await pool.connect();
     console.log(`Connected to database ${process.env.PGDATABASE} on port ${process.env.PGPORT}`);
+    pool.query(sql);
   } catch(err) {
-    console.log(err);
-  }
+    if (err.code === '3D000') {
+      console.log(`Database 'babycoin' doesn't exist. Creating database 'babycoin'.`)
+      const client = new Client({ database: process.env.USER });
+      await client.connect();
+      await client.query('CREATE DATABASE babycoin');
+      client.end();
+      const pool = new Pool();
+      console.log(`Connected to database ${process.env.PGDATABASE} on port ${process.env.PGPORT}`);
+      pool.query(sql);
+    } 
+  } 
 })();
+
+
+// db.query(sql, (err, res) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log('SQL file loaded successfully');
+//   }
+// });
+
+// (async() => {
+//   try {
+//     await pool.connect();
+//     console.log(`Connected to database ${process.env.PGDATABASE} on port ${process.env.PGPORT}`);
+//     await pool.query(sql)
+//   } catch(err) {
+//     console.log(err);
+//   }
+// })();
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,19 @@
--- ADD YOUR DATABASE SCHEMAS
+CREATE TABLE IF NOT EXISTS trader (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS achievements (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  lesson VARCHAR(255) NOT NULL,
+  points INT NOT NULL,
+  icon VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS trader_achievements (
+  id SERIAL PRIMARY KEY,
+  trader_id INTEGER REFERENCES trader(id),
+  achievement_id INTEGER REFERENCES achievements(id)
+);
