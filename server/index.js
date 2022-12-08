8e062f15a4430f938a60dc17d591c307015e61b2
Nick Hays <nicholasahays@gmail.com>
2022-12-08 11:51:47 -0700

    Fixes typos and updates import of home controllers

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('../db/index.js'); //tests db connection
 const express = require('express');
 const app = express();
 const port = 3000;
-const home = require('./controllers/Home/home.js');
+const { home } = require('./controllers/controllers.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
@@ -12,13 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.post('/users/:id/transactions/buy', (req, res) => {
   // Buy transaction takes place
   // Beginning code for home profile section
-  home.postBuyPortofolioUpdate(req, res);
+  home.postBuyPortfolioUpdate(req, res);
 });
 
 app.post('/users/:id/transactions/sell', (req, res) => {
   // Buy transaction takes place
   // Beginning code for home profile section
-  home.postSellPortofolioUpdate(req, res);
+  home.postSellPortfolioUpdate(req, res);
 });
 
 
