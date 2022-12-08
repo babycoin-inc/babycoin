88ab1d96d1b3f738d6e02c6cff0d2bf271d1a99f
Nick Hays <nicholasahays@gmail.com>
2022-12-08 13:29:22 -0700

    Checks username availability and registers user, using try catch

--- server/controllers/Login/signup.js
+++ server/controllers/Login/signup.js
@@ -1,15 +1,19 @@
-const { isUsernameAvailable } = require('../../../models/Login/signup.js');
+const { isUsernameAvailable, registerUser } = require('../../../models/Login/signup.js');
 const encryptPassword = require('./passwordHelper.js');
 
-const signup = (req, res) => {
+const signup = async (req, res) => {
   const { username, password } = req.body;
-  isUsernameAvailable(username)
-    .then(usernameAvailable => {
-      if(!usernameAvailable) return res.status(409).send('Username Already Exists');
-      const encryptedPassword = encryptPassword(password);
-      res.send('Route under construction');
-    })
-    .catch(err => console.log(err));
+  const encryptedPassword = encryptPassword(password);
+
+  try {
+    const usernameAvailable = await isUsernameAvailable(username);
+    if(!usernameAvailable) res.status(409).send('Username Already Exists');
+    await registerUser(username, encryptedPassword);
+    res.status(201).send('Account Created'):
+  } catch (err) {
+    console.log(err);
+  }
+
 }
 
 module.exports = {
