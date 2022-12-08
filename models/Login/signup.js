10b88046d571ca5aa24533adbc420417e2ea95a9
Nick Hays <nicholasahays@gmail.com>
2022-12-08 13:29:50 -0700

    Adds registerUser function that inserts username and password to db

--- models/Login/signup.js
+++ models/Login/signup.js
@@ -1,13 +1,14 @@
 const pool = require('../db/index.js');
 
 const SignUp = {
+  registerUser: (username, password) => pool.query('INSERT INTO member(username, password) values($1, $2)', [username, password]),
   isUsernameAvailable: (username) => {
     return (
       pool
-        .query('SELECT EXISTS(SELECT 1 FROM authentication WHERE username = $1))', [username])
+        .query('SELECT EXISTS(SELECT 1 FROM member WHERE username = $1))', [username])
         .then(result => result.rows[0].exists);
-    )
+    );
   },
-}
+};
 
 module.exports = SignUp;
