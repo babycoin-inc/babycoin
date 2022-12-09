6a27fac446629cf2a6e691a026729d68ca4cf134
Nick Hays <nicholasahays@gmail.com>
2022-12-09 00:16:39 -0700

    Exports

---
+++ models/Login/models.js
@@ -0,0 +1,7 @@
+const login = require('./login.js');
+const signup = require('./signup.js');
+
+module.exports = {
+  login,
+  signup,
+}
---
+++ models/models.js
@@ -0,0 +1,5 @@
+const Login = require('./Login/models.js');
+
+module.exports = {
+  Login,
+}
