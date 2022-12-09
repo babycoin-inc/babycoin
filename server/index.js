aaccd5fdcdedda01248e610e7a3ffa382c079677
Nick Hays <nicholasahays@gmail.com>
2022-12-09 14:06:27 -0700

    Comments out db and other routes as there is an error with an id column in one of the tables

--- server/index.js
+++ server/index.js
@@ -1,18 +1,35 @@
-require('../db/index.js'); //tests db connection
+// require('../db/index.js'); //tests db connection
+require('../passport.config.js');
 const express = require('express');
 const passport = require('passport');
-const flash = require('express-flash');
-require('../passport.config.js');
 const app = express();
-const port = 3000;
-const { home } = require('./controllers/controllers.js');
+const { PORT } = process.env;
+
+const flash = require('express-flash');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 /**Controllers */
 const { signup } = require('./controllers/Login/controllers.js');
+// const { home } = require('./controllers/controllers.js');
 
 app.use(passport.initialize());
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET))
+const whitelist = process.env.WHITELISTED_DOMAINS ? process.env.WHITELISTED_DOMAINS.split(',') : [];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+
+  credentials: true,
+}
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/signup', signup);
@@ -24,10 +41,10 @@ app.post('/login', passport.authenticate('local', {
 
 });
 
-app.post('/users/:id/transactions/buy', home.postBuyPortfolioUpdate);
-app.post('/users/:id/transactions/sell', home.postSellPortfolioUpdate);
+// app.post('/users/:id/transactions/buy', home.postBuyPortfolioUpdate);
+// app.post('/users/:id/transactions/sell', home.postSellPortfolioUpdate);
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
