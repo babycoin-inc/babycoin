ef3295725928f47080fb6768fc3d7a502ce626aa
Nick Hays <nicholasahays@gmail.com>
2022-12-09 00:12:48 -0700

    Setting up passport.authenticate() middleware for login route

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
 require('../db/index.js'); //tests db connection
 const express = require('express');
+const passport = require('passport');
+const flash = require('express-flash');
+require('../passport.config.js');
 const app = express();
 const port = 3000;
 
 /**Controllers */
 const { signup } = require('./controllers/Login/controllers.js');
 
+app.use(passport.initialize());
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/signup', signup);
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}), (req, res) => {
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
