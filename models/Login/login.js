7bd5a2cf664c9254cb876f39cf378f28e5c723d9
Nick Hays <nicholasahays@gmail.com>
2022-12-09 16:05:14 -0700

    Changes table name from member to users

--- models/Login/login.js
+++ models/Login/login.js
@@ -3,12 +3,12 @@ const pool = require('../../db/index.js');
 const Login = {
   getEncryptedPassword: (username) => {
     return pool
-      .query('SELECT password FROM member WHERE username = $1', [username])
+      .query('SELECT password FROM users WHERE username = $1', [username])
       .then(result => result.rows[0].password);
   },
   getUser: (username) => {
     return pool
-      .query('SELECT * FROM member WHERE username = $1', [username])
+      .query('SELECT id, username, password FROM users WHERE username = $1', [username])
       .then(result => result.rows[0] || false);
   }
 }
