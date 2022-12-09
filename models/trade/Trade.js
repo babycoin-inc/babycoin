4972cfdad0d71898ad3740b42489555e3d37c930
Duke Romkey <dukeromkey@gmail.com>
2022-12-09 00:07:52 -0600

    added instructions on order

--- models/trade/Trade.js
+++ models/trade/Trade.js
@@ -1,10 +1,18 @@
 const { query } = require('../../db/index.js');
 
 exports.fulfillBuyOrder = (id) => {
+  // subtract total_trade_fiat from cash
+  // add total_trade_coin to portfolio
+  // calculations for dollar cost
+  // calculations for avg price
   return 'fulfilled buy order';
 };
 
 exports.fulfillSellOrder = (id) => {
+  // add total_trade_fiat from cash
+  // subtract total_trade_coin to portfolio
+  // calculations for dollar cost
+  // calculations for avg price
   return 'fulfilled sell order';
 };
 
