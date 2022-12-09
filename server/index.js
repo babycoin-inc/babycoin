5ef8737363a5ccc330672a5b86087b9856444dfa
Alex Yeung <alexyeung@Rogelios-MacBook-Pro.local>
2022-12-09 11:23:19 +0800

    Updated Alex2

--- server/index.js
+++ server/index.js
@@ -1,28 +1,17 @@
 require('../db/index.js'); //tests db connection
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
-const port = 3000;
-const { home } = require('./controllers/controllers.js');
-=======
 const port = 4000;
->>>>>>> Alex-1
+const { home } = require('./controllers/controllers.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-<<<<<<< HEAD
-
-app.post('/users/:id/transactions/buy', home.postBuyPortfolioUpdate);
-app.post('/users/:id/transactions/sell', home.postSellPortfolioUpdate);
-
-=======
 //newsfeed
 app.get("/newsfeed", (req, res) => {
   nfserver.getAll(console.log);
 });
->>>>>>> Alex-1
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
