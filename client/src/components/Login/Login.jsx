f0e5167a9db9eed6c007e1f0c520851b7d1be0a6
Nick Hays <nicholasahays@gmail.com>
2022-12-10 06:23:19 -0700

    Adds sign up section and respective handlers

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,17 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 import babycoin from '../../../dist/assets/BabyCoin.png'
 import bgImage from '../../../dist/assets/trading-bg.jpg'
 import { FcGoogle } from 'react-icons/fc'
-import axios from 'axios'
+import axios from 'axios';
 
-const Login = () => {
+const Login = ({ updateUser }) => {
 
-  const handleLogin = () => {
-    axios
-      .post('auth/login')
-      .then({ data } => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordComparison, setPasswordComparison] = useState('')
 
+  const handleInput = (e) => {
+    e.preventDefault();
+    if(e.target.id === 'username') {
+      setUsername(e.target.value);
+    } else if (e.target.id === 'password') {
+      setPassword(e.target.value);
+    } else {
+      setPasswordComparison(e.target.value);
+    }
+    console.log('USERNAME: ', username);
+    console.log('PASSWORD: ', password);
+    console.log('CONFIRM PASSWORD: ', passwordComparison)
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios.post('/login', {
+        username: username,
+        password: password
       })
+      .then(({ data }) => {
+        console.log('DATA FROM SERVER: ', data);
+        updateUser(data);
+      })
+      .catch(err => console.log(err))
+  }
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    if(password !== passwordComparison) {
+      return alert('Passwords must match');
+    }
+    axios.post('/signup', {
+      username: username,
+      password: password
+    })
+    .catch(err => console.log(err));
   }
 
   return (
@@ -28,14 +63,27 @@ const Login = () => {
           </div>
           <div className='flex flex-col mb-4'>
             <label>Username</label>
-            <input className='border relative bg-gray-100 p-2' type='text'/>
+            <input id='username' className='border relative bg-gray-100 p-2' type='text' onChange={handleInput}/>
           </div>
           <div className='flex flex-col'>
             <label>Password</label>
-            <input className='border relative bg-gray-100 p-2' type='text'/>
+            <input id='password' className='border relative bg-gray-100 p-2' type='text' onChange={handleInput}/>
           </div>
           <button className='w-full py-3 mt-8 bg-blue-600 hover:bg-blue-500 relative text-white' onClick={handleLogin}>Log In</button>
           <p className='text-center mt-8'>Not a Member? Sign Up Now!</p>
+          <div className='flex flex-col mb-4'>
+            <label>Username</label>
+            <input id='username' className='border relative bg-gray-100 p-2' type='text' onChange={handleInput}/>
+          </div>
+          <div className='flex flex-col'>
+            <label>Password</label>
+            <input id='password' className='border relative bg-gray-100 p-2' type='text' onChange={handleInput}/>
+          </div>
+          <div className='flex flex-col'>
+            <label>Confirm Password</label>
+            <input id='passwordcomparison' className='border relative bg-gray-100 p-2' type='text' onChange={handleInput}/>
+          </div>
+          <button className='w-full py-3 mt-8 bg-blue-600 hover:bg-blue-500 relative text-white' onClick={handleSignUp}>Sign Up</button>
         </form>
       </div>
     </div>
