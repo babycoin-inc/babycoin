cc7267b4cad9abb791472af52917b7a8a31b5055
Nick Hays <nicholasahays@gmail.com>
2022-12-09 20:00:06 -0700

    Attempting auth without passport

--- server/controllers/Login/controllers.js
+++ server/controllers/Login/controllers.js
@@ -1,9 +1,11 @@
 const { signup } = require('./signup.js');
 const { login } = require('./login.js')
 const refreshToken = require('./refreshToken.js');
+const verifyToken = require('./verifyToken');
 
 module.exports = {
   signup,
   login,
-  refreshToken
+  refreshToken,
+  verifyToken
 };
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // require('../db/index.js'); //tests db connection
-require('../passport.config.js');
+// require('../passport.config.js');
 const express = require('express');
-const passport = require('passport');
+// const passport = require('passport');
 const app = express();
 const { PORT } = process.env;
 
@@ -10,36 +10,36 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 /**Controllers */
-const { signup } = require('./controllers/Login/controllers.js');
+const { signup, login, verifyToken, refreshToken } = require('./controllers/Login/controllers.js');
 // const { home } = require('./controllers/controllers.js');
 
-app.use(passport.initialize());
+// app.use(passport.initialize());
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET))
-const whitelist = process.env.WHITELISTED_DOMAINS ? process.env.WHITELISTED_DOMAINS.split(',') : [];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
+// const whitelist = process.env.WHITELISTED_DOMAINS ? process.env.WHITELISTED_DOMAINS.split(',') : [];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error("Not allowed by CORS"))
+//     }
+//   },
 
-  credentials: true,
-}
+//   credentials: true,
+// }
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/signup', signup);
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}), (req, res) => {
+// app.post('/login', passport.authenticate('local', {
+//   successRedirect: '/',
+//   failureRedirect: '/login',
+//   failureFlash: true
+// }), (req, res) => {
 
-});
+// });
 
 // app.post('/users/:id/transactions/buy', home.postBuyPortfolioUpdate);
 // app.post('/users/:id/transactions/sell', home.postSellPortfolioUpdate);
