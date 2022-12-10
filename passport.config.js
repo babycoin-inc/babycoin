3bcc8c0fa0494c7a5b877ad7843b9077adeada16
Nick Hays <nicholasahays@gmail.com>
2022-12-10 06:11:22 -0700

    Removed getUserById from deserializeUser, this was left over from a previous configuration and caused an invalid input syntax for uuid error

--- passport.config.js
+++ passport.config.js
@@ -1,6 +1,30 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const { authenticateUser } = require('./server/controllers/controllers.js')
+const { Login } = require('./models/Login/models.js')
+const { comparePasswords } = require('./server/controllers/Login/passwordHelper.js');
+
+const authenticateUser = async(username, password, done) => {
+  try {
+    const user = await Login.getUser(username);
+    if(!user) return done(null, false, { message: 'Unable to login with Username and Password' });
+    const encryptedPassword = await Login.getEncryptedPassword(username);
+    const doPasswordsMatch = await comparePasswords(password, encryptedPassword);
+    if(!doPasswordsMatch) return done(null, false, { message: 'Unable to login with Username and Password' });
+    return done(null, user);
+  } catch(err) {
+    return done(err);
+  }
+};
+
+passport.serializeUser((user, done) => {
+  // done(null, user.id);
+  done(null, user);
+})
+
+passport.deserializeUser(async(user, done) => {
+  // const user = await Login.getUserById(id);
+  return done(null, user);
+})
 
 // const initialize = (passport) => {
 
@@ -9,3 +33,4 @@ const { authenticateUser } = require('./server/controllers/controllers.js')
 // }
 
 // module.exports = initialize;
+module.exports = passport;
