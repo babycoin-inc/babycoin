5539eb701753d9f890dbe6c8cc8323598e1505d4
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-12-10 14:54:21 -0600

    Add try/catch to controllers

--- server/controllers/Achievements/achievements.js
+++ server/controllers/Achievements/achievements.js
@@ -1,24 +1,36 @@
 const { Achievements } = require("../../../models/models.js");
 
 exports.getAchievements = async (req, res) => {
-    const achievements = await Achievements.getAchievements();
-    res.send(achievements.rows);
+    try {
+        const achievements = await Achievements.getAchievements();
+        res.send(achievements.rows);
+    } catch (error) {
+        console.log('Error achievement:', error);
+        res.status(404).end();
+    }
 };
 
 exports.getUserAchievements = async (req, res) => {
     const { trader_id } = req.params;
-    const achievements = await Achievements.getUserAchievements(trader_id);
-    res.send(achievements.rows);
+
+    try {
+        const achievements = await Achievements.getUserAchievements(trader_id);
+        res.send(achievements.rows);
+    } catch (error) {
+        console.log('Error achievement:', error);
+        res.status(404).end();
+    }
 };
 
 exports.addUserAchievement = async (req, res) => {
     const { trader_id } = req.params;
     const { achievement } = req.body;
+    
     try {
         await Achievements.addUserAchievement(trader_id, achievement);
         res.status(201).end();
     } catch (error) {
-        console.log('Error adding user achievement:', error);
+        console.log('Error achievement:', error);
         res.status(404).end();
     }
 };
