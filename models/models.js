072d1d286525c82b5464aa9e70bd4a8b12e5a412
Abdiel Sanchez <92281407+absaga@users.noreply.github.com>
2022-12-10 11:32:11 -0600

    Revert "Watchlist"

--- models/models.js
+++ models/models.js
@@ -1,7 +1,6 @@
 const Home = require('./home/Home.js');
 const Trade = require('./trade/trade.js');
-const Market = require('./marketWatch/marketWatch.js');
 
 module.exports = {
-  Home, Trade, Market
+  Home, Trade
 }
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('../db/index.js'); //tests db connection
 const express = require('express');
 const app = express();
 const port = 3000;
-const { nf, home, trade, market} = require('./controllers/controllers.js');
+const { nf, home, trade } = require('./controllers/controllers.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
@@ -23,8 +23,6 @@ app.get("/newsfeed", (req, res) => {
     }
   })
 });
-
-app.get('/coins/markets', market.getCoins);
 
 app.get("/nfAPI", (req, res) => {
   nf.runAPI((err,result) => {
