95ac66e7b6163e326a94b597e4661dfaaaaacf0a
Duke Romkey <dukeromkey@gmail.com>
2022-12-10 00:14:00 -0600

    allCoins route fetching proper information

--- models/home/Home.js
+++ models/home/Home.js
@@ -8,6 +8,10 @@ exports.calcDollarCost = (id) => {
 
 };
 
-exports.allCoins = (user_id) => {
-  return 'Test all coins';
+exports.allCoins = async (user_id) => {
+  const portfolio = await query(`SELECT json_build_object('coin', c.name, 'acronym', c.acronym, 'value', (p.quantity * c.latest_price), 'quantity', p.quantity, 'avg_entry', p.avg_price, 'curr_price', c.latest_price, 'profit_loss', ((p.quantity * c.latest_price) - p.dollar_cost), 'image', c.image) FROM coins c INNER JOIN portfolio p ON c.id = p.coin_id WHERE trader_id = $1;`, [user_id]);
+  let formatted = portfolio.rows.map((coin) => {
+    return coin.json_build_object;
+  });
+  return formatted;
 };
