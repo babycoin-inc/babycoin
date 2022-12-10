3dd506cfc49e5e1f82fd7f52025c580bc72ed4e9
Duke Romkey <dukeromkey@gmail.com>
2022-12-10 13:09:53 -0600

    Adding code

---
+++ server/controllers/MarketWatch/marketwatch.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const marketWatch = require('../../../models/marketWatch/marketWatch.js');
+const axios = require('axios');
+
+exports.getCoins = (req, res) => {
+  // get all crypto coins
+  return axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,polkadot,tether,binancecoin,cardano,dogecoin,ripple,solana&order=market_cap_desc&per_page=12&page=1&sparkline=false',
+  {
+    headers: { "Accept-Encoding": "gzip,deflate,compress" }
+  })
+ .then(result => {
+    marketWatch.writeCoins(result.data);
+    res.send(result.data);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(404).send('Error');
+  })
+}
