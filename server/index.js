30272b4511658928e74985ac9bace93e5264a9ab
Nick Hays <nicholasahays@gmail.com>
2022-12-10 06:17:51 -0700

    Removes unnecessary middleware and comments

--- server/index.js
+++ server/index.js
@@ -1,21 +1,19 @@
 // require('../db/index.js'); //tests db connection
-// require('../passport.config.js');
+require('../passport.config.js');
 const express = require('express');
 require('dotenv').config();
-// const passport = require('passport');
+const passport = require('passport');
+
 const app = express();
 const PORT = process.env.PORT;
 
 const flash = require('express-flash');
-// const cookieParser = require('cookie-parser');
-// const cors = require('cors');
 const session = require('cookie-session');
 
 /**Controllers */
 const { signup, login, verifyToken, refreshToken } = require('./controllers/Login/controllers.js');
 // const { home } = require('./controllers/controllers.js');
 
-// app.use(passport.initialize());
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,31 +22,15 @@ app.use(session({
   secret: 'secret',
   expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
 }));
+app.use(passport.initialize())
+app.use(passport.session())
 
-// app.use(cookieParser(process.env.COOKIE_SECRET))
-// const whitelist = process.env.WHITELISTED_DOMAINS ? process.env.WHITELISTED_DOMAINS.split(',') : [];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin || whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error("Not allowed by CORS"))
-//     }
-//   },
+app.post('/signup', signup);
+app.post('/login', (req, res, next) => {
+  console.log(req.body);
+  next();
+}, passport.authenticate('local'), login);
 
-//   credentials: true,
-// }
-// app.use(cors(corsOptions));
-
-app.post('/auth/signup', signup);
-app.post('/auth/login', login);
-// app.post('/login', passport.authenticate('local', {
-//   successRedirect: '/',
-//   failureRedirect: '/login',
-//   failureFlash: true
-// }), (req, res) => {
-
-// });
 
 // app.post('/users/:id/transactions/buy', home.postBuyPortfolioUpdate);
 // app.post('/users/:id/transactions/sell', home.postSellPortfolioUpdate);
