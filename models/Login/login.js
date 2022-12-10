7f8e635193a4ee084f2d04cb2acc14e49109917b
Nick Hays <nicholasahays@gmail.com>
2022-12-09 17:20:31 -0700

    Adds getUserByToken function

--- models/Login/login.js
+++ models/Login/login.js
@@ -11,6 +11,10 @@ const Login = {
       .query('SELECT id, username, password FROM users WHERE username = $1', [username])
       .then(result => result.rows[0] || false);
   },
+  getUserByToken: (token) => {
+    return pool
+      .query('SELECT id, username, password FROM users WHERE token = $1', [token]);
+  },
   updateToken: (token, id) => {
     return pool
       .query('UPDATE users SET refresh_token = $1 WHERE id = $2', [token, id]);
