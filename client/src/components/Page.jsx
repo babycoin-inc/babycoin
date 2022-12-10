8e8af0a3301c0479b928af5c8da1ede086720bab
Nick Hays <nicholasahays@gmail.com>
2022-12-10 06:22:37 -0700

    Uses Page for standin of how I will set up App and Login components

--- client/src/components/Page.jsx
+++ client/src/components/Page.jsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import App from './App.jsx';
 import Login from './Login/Login.jsx';
 
-
-
 const Page = () => {
-  const [authorizedUser, setAuthorizedUser] = useState(false);
+  const [authorizedUser, setAuthorizedUser] = useState(null);
 
-  if(authorizedUser === null) {
-    return <div>Loading...</div>
-  }
-  if(authorizedUser) {
+  // return (
+  //   {authorizedUser ? <App /> : <Login updateUser={setAuthorizedUser}/>}
+  // )
+
+  if(!authorizedUser) {
+    return <Login updateUser={setAuthorizedUser}/>
+  } else {
     return <App />
   }
-  return <Login />
 }
 
 export default Page;
