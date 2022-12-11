1b27bd9cd10aa2d672039775ca0458228793da48
Nick Hays <nicholasahays@gmail.com>
2022-12-11 16:43:49 -0700

    Renams Login to Auth

---
+++ models/Auth/login.js
@@ -0,0 +1,31 @@
+const pool = require('../../db/index.js');
+
+const Login = {
+  getHash: (username) => {
+    return pool
+      .query('SELECT password FROM users WHERE username = $1', [username])
+      .then(result => result.rows[0].password);
+  },
+  getUser: (username) => {
+    return pool
+      .query('SELECT id, username, password FROM users WHERE username = $1', [username])
+      .then(result => result.rows[0] || null);
+  },
+  getUserByToken: (token) => {
+    return pool
+      .query('SELECT id, username, password FROM users WHERE refresh_token = $1', [token])
+      .then(result => result.rows[0] || null);
+  },
+  getUserById: (id) => {
+    return pool
+      .query('SELECT id, username, password FROM users WHERE id = $1', [id])
+      .then(result => result.rows[0] || null);
+  },
+  updateToken: (token, id) => {
+    return pool
+      .query('UPDATE users SET refresh_token = $1 WHERE id = $2', [token, id]);
+  },
+}
+
+
+module.exports = Login;
---
+++ models/Auth/models.js
@@ -0,0 +1,7 @@
+const Login = require('./login.js');
+const signup = require('./signup.js');
+
+module.exports = {
+  Login,
+  signup,
+}
