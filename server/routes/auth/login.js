a9c8888e001eac1f07a530dee0eb24e485b28b77
Nick Hays <nicholasahays@gmail.com>
2022-12-11 16:42:49 -0700

    Adds routes directory that houses app routes and auth routes

---
+++ server/routes/auth/login.js
@@ -0,0 +1,8 @@
+const Router = require('express');
+const { auth } = require('../../controllers/controllers.js')
+
+const router = Router();
+
+router.post('/', auth.login)
+
+module.exports = router;
---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+const Router = require('express');
+const appRouter = require('./app/index.js');
+const authRouter = require('./auth/index.js');
+
+const router = Router();
+
+router.use('/', appRouter);
+router.use('/auth', authRouter);
+
+export default router;
