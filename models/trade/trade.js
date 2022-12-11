8b45ac4bd45887a2bc8e7013166bc94c5b4c4648
Duke Romkey <dukeromkey@gmail.com>
2022-12-11 15:40:17 -0600

    Added db logic for post buy and sell transactions

--- models/trade/trade.js
+++ models/trade/trade.js
@@ -30,17 +30,17 @@ exports.insertSellTransaction = (transaction, user_id) => {
 
 exports.fulfillBuyTransaction = async (transaction, user_id) => {
   // Subtract total_trade_fiat and update dollar cost for CASH
-  query(`UPDATE portfolio SET quantity = (quantity - $1), dollar_cost = (dollar_cost - $1) WHERE coin_id = $2 AND trader_id = $3;`, [transaction.total_trade_fiat, transaction.coin_id, user_id]);
+  query(`UPDATE portfolio SET quantity = (quantity - $1), dollar_cost = (dollar_cost - $1) WHERE coin_id = (SELECT id FROM coins WHERE acronym = 'usd') AND trader_id = $2;`, [transaction.total_trade_fiat, user_id]);
   // add total_trade_coin to portfolio || Dollar Cost || Avg Price
   query(`INSERT INTO portfolio(trader_id, coin_id, dollar_cost, avg_price, quantity) VALUES($1, $2, $3, $4, $5) ON CONFLICT (trader_id, coin_id) DO UPDATE SET quantity = (portfolio.quantity + $5), dollar_cost = (portfolio.dollar_cost + $3), avg_price = ((portfolio.dollar_cost + $3) / (portfolio.quantity + $5)) WHERE portfolio.coin_id = $2 AND portfolio.trader_id = $1;`, [user_id, transaction.coin_id, transaction.total_trade_fiat, transaction.purchase_price, transaction.total_trade_coin]);
 
   return 'Hit Fulfill Buy Transaction';
 }
 
-exports.fulfillSellTransaction = (transaction, user_id) => {
+exports.fulfillSellTransaction = (transaction) => {
   // add total_trade_fiat from cash
-  // subtract total_trade_coin to portfolio
-  // calculations for dollar cost
-  // calculations for avg price
+  query(`UPDATE portfolio SET quantity = (quantity + $1), dollar_cost = (dollar_cost + $1) WHERE coin_id = (SELECT id FROM coins WHERE acronym = 'usd') AND trader_id = $2;`, [transaction.total_trade_fiat, user_id]);
+  // subtract total_trade_coin to portfolio || Dollar Cost || Avg Price
+  query(`UPDATE portfolio SET quantity = (quantity - $1), dollar_cost = (dollar_cost - ($1 * $2)), avg_price = (dollar_cost - ($1 * $2)) / (quantity - $1) WHERE coin_id = $3 AND trader_id = 1;`, [transaction.total_trade_coin, transaction.purchase_price, transaction.coin_id, user_id]);
   return 'Hit Fulfill Sell Transaction';
 }
