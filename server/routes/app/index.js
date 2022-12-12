9d9433f84806bedf85e92490b672495e457a8815
Nick Hays <nicholasahays@gmail.com>
2022-12-11 23:35:23 -0700

    Using JWT without passport

--- server/routes/app/index.js
+++ server/routes/app/index.js
@@ -1,5 +1,26 @@
+require('dotenv').config();
 const { Router } = require('express');
+const jwt = require('jsonwebtoken');
 
 const router = Router();
+
+router.get('/', (req, res) => {
+  try {
+    console.log(req.headers.authorization?.split(' '));
+    const bearerToken = req.headers.authorization?.split(' '); // ['Bearer', <token>]
+    const token = bearerToken && bearerToken[0] === 'Bearer' ? bearerToken[1] : null;
+    if(!bearerToken || !token) {
+      res.status(401).json({ message: 'unauthorized' });
+      return;
+    }
+    const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    console.log(payload);
+    res.json({message: 'Authorized Token'});
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({message: 'Something went wrong'});
+  }
+
+})
 
 module.exports = router;
