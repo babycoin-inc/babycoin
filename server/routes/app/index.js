62ebddc90d44d7ec58f715903ef648a263424c6b
Nick Hays <nicholasahays@gmail.com>
2022-12-11 23:36:49 -0700

    Using JWT strategy with passport

--- server/routes/app/index.js
+++ server/routes/app/index.js
@@ -1,20 +1,11 @@
 require('dotenv').config();
 const { Router } = require('express');
-const jwt = require('jsonwebtoken');
+const { authenticate } = require('passport');
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', authenticate('jwt'), (req, res) => {
   try {
-    console.log(req.headers.authorization?.split(' '));
-    const bearerToken = req.headers.authorization?.split(' '); // ['Bearer', <token>]
-    const token = bearerToken && bearerToken[0] === 'Bearer' ? bearerToken[1] : null;
-    if(!bearerToken || !token) {
-      res.status(401).json({ message: 'unauthorized' });
-      return;
-    }
-    const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    console.log(payload);
     res.json({message: 'Authorized Token'});
   } catch(err) {
     console.log(err);
