6fd3dbe76b8d25d53cc1ca7b01a3265488c18a1f
Nick Hays <nicholasahays@gmail.com>
2022-12-12 02:33:42 -0700

    Creates token upon sign up, which gives user authorization status (can login upon sign up)

--- server/controllers/Auth/signup.js
+++ server/controllers/Auth/signup.js
@@ -1,15 +1,18 @@
-const { isUsernameUnavailable, registerUser } = require('../../../models/Login/signup.js');
-const { hashPassword } = require('./passwordHelper.js');
+const { Signup } = require('../../../models/models.js').Auth;
+const { hashPassword } = require('../../utils/passwords.js');
+const { createToken } = require('../../utils/userAuth.js');
 
 const signup = async (req, res) => {
   const { username, password } = req.body;
   const hash = hashPassword(password);
 
   try {
-    const usernameUnavailable = await isUsernameUnavailable(username);
-    if(usernameUnavailable) return res.status(409).send('Username Already Exists');
-    await registerUser(username, hash);
-    res.status(201).send('Account Created');
+    const usernameUnavailable = await Signup.isUsernameUnavailable(username);
+    if(usernameUnavailable) return res.status(409).json('Username Already Exists');
+    const id = await Signup.registerUser(username, hash);
+    const token = createToken(id, username);
+    const authorizedUser = { username, id, token };
+    res.status(201).json(authorizedUser);
   } catch (err) {
     console.log(err);
   }
