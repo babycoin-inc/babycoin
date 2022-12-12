ce3ea95203af75079d13af51a96b6b50f24ca34b
Nick Hays <nicholasahays@gmail.com>
2022-12-12 02:34:47 -0700

    Creates token on login and sends back username, id, and token

--- server/controllers/Auth/login.js
+++ server/controllers/Auth/login.js
@@ -1,13 +1,10 @@
-
+const { createToken } = require('../../utils/userAuth.js');
 
 const login = (req, res) => {
-
-  const authorizedUser = {
-    id: req.user.id,
-    username: req.user.username
-  };
-  res.status(200).send(authorizedUser);
-
+  const { id, username } = req.user;
+  const token = createToken(id, username);
+  const authorizedUser = { id, username, token };
+  res.status(200).json(authorizedUser);
 }
 
 module.exports = login;
