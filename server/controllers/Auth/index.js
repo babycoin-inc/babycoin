715c41d1ec2598f3c094d46247a889399e5c964b
Nick Hays <nicholasahays@gmail.com>
2022-12-12 20:23:58 -0700

    Moves controllers to one file

--- server/controllers/Auth/index.js
+++ server/controllers/Auth/index.js
@@ -1,11 +1,29 @@
-const { signup } = require('./signup.js');
-const login = require('./login.js')
-const refreshToken = require('./refreshToken.js');
-const verifyToken = require('./verifyToken');
+const { Auth } = require('../../../models/models.js');
+const { isUsernameUnavailable, registerUser } = Auth.Signup;
+const { hashPassword } = require('../../utils/passwords.js');
 
-module.exports = {
-  signup,
-  login,
-  refreshToken,
-  verifyToken
-};
+const AuthControllers = {
+  signupController: async (req, res) => {
+    const { username, password } = req.body;
+    const encryptedPassword = hashPassword(password);
+
+    try {
+      const usernameUnavailable = await isUsernameUnavailable(username);
+      if(usernameUnavailable) return res.status(409).send('Username Already Exists');
+      await registerUser(username, encryptedPassword);
+      res.status(201).send('Account Created');
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  loginController: (req, res) => {
+    console.log('REQ.USER', req.user)
+    const authorizedUser = {
+      id: req.user.id,
+      username: req.user.username
+    };
+    res.status(200).send(authorizedUser);
+  }
+}
+
+module.exports = AuthControllers;
