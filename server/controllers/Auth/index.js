192aec779b0050bcea71d0e5cc0173a5bc45b9f6
Nick Hays <nicholasahays@gmail.com>
2022-12-12 23:38:41 -0700

    Creates token in loginController and sends back authorized user with token, still having Cannot read properties of undefined (reading 'fromAuthHeaderAsBearerToken')

--- server/controllers/Auth/index.js
+++ server/controllers/Auth/index.js
@@ -1,6 +1,7 @@
 const { Auth } = require('../../../models/models.js');
 const { isUsernameUnavailable, registerUser } = Auth;
 const { hashPassword } = require('../../utils/passwords.js');
+const createToken = require('../../utils/tokens.js');
 
 const AuthControllers = {
   signupController: async (req, res) => {
@@ -18,10 +19,9 @@ const AuthControllers = {
   },
   loginController: (req, res) => {
     console.log('REQ.USER', req.user)
-    const authorizedUser = {
-      id: req.user.id,
-      username: req.user.username
-    };
+    const { id, username } = req.user;
+    const token = createToken(id, username);
+    const authorizedUser = { id, username, token };
     res.status(200).send(authorizedUser);
   }
 }
