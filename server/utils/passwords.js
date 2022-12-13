51105a42f37431cf7fb6b0ae6861c2ec66a3ea10
Nick Hays <nicholasahays@gmail.com>
2022-12-12 21:26:13 -0700

    Updates imports

--- server/utils/passwords.js
+++ server/utils/passwords.js
@@ -3,7 +3,7 @@ const SALT_ROUNDS = 12;
 const { Auth } = require('../../models/models.js');
 
 const hashPassword = (password) => bcrypt.hashSync(password, SALT_ROUNDS);
-const getHash = (username) => Auth.Login.getHash(username);
+const getHash = (username) => Auth.getHash(username);
 const comparePasswords = (password, hash) => bcrypt.compare(password, hash)
 
 module.exports = {
