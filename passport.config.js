ca0c153fc0c2e79135cf3dd0376d8709c788d458
Nick Hays <nicholasahays@gmail.com>
2022-12-12 20:19:25 -0700

    Updates imports, changes encryptedPassword to hash, and adds delete password to serializeUser

--- passport.config.js
+++ passport.config.js
@@ -1,14 +1,14 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const { Login } = require('./models/Login/models.js')
-const { comparePasswords } = require('./server/controllers/Login/passwordHelper.js');
+const { Auth } = require('./models/models.js')
+const { comparePasswords } = require('./server/utils/passwords.js');
 
 const authenticateUser = async(username, password, done) => {
   try {
-    const user = await Login.getUser(username);
+    const user = await Auth.Login.getUser(username);
     if(!user) return done(null, false, { message: 'Unable to login with Username and Password' });
-    const encryptedPassword = await Login.getEncryptedPassword(username);
-    const doPasswordsMatch = await comparePasswords(password, encryptedPassword);
+    const hash = await Auth.Login.getHash(username);
+    const doPasswordsMatch = await comparePasswords(password, hash);
     if(!doPasswordsMatch) return done(null, false, { message: 'Unable to login with Username and Password' });
     return done(null, user);
   } catch(err) {
@@ -17,11 +17,12 @@ const authenticateUser = async(username, password, done) => {
 };
 
 passport.serializeUser((user, done) => {
+  if(user.password) delete user.password;
   done(null, user);
 })
 
 passport.deserializeUser((user, done) => {
-  return done(null, user);
+  done(null, user);
 })
 
 passport.use(new LocalStrategy(authenticateUser));
