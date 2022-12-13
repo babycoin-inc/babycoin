53c8e3d22f35f34e7ec79b4082b580691004e2ab
Duke Romkey <dukeromkey@gmail.com>
2022-12-12 20:50:12 -0600

    Modified buy/sell transaction (2nd half) to be a db transaction format

--- models/trade/trade.js
+++ models/trade/trade.js
@@ -1,4 +1,13 @@
 var {query} = require('../../db/index.js');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+});
 
 exports.getTransactions = (id) => {
 
@@ -29,18 +38,43 @@ exports.insertSellTransaction = (transaction, user_id) => {
 
 
 exports.fulfillBuyTransaction = async (transaction, user_id) => {
-  // Subtract total_trade_fiat and update dollar cost for CASH
-  query(`UPDATE portfolio SET quantity = (quantity - $1), dollar_cost = (dollar_cost - $1) WHERE coin_id = (SELECT id FROM coins WHERE acronym = 'usd') AND trader_id = $2;`, [transaction.total_trade_fiat, user_id]);
-  // add total_trade_coin to portfolio || Dollar Cost || Avg Price
-  query(`INSERT INTO portfolio(trader_id, coin_id, dollar_cost, avg_price, quantity) VALUES($1, $2, $3, $4, $5) ON CONFLICT (trader_id, coin_id) DO UPDATE SET quantity = (portfolio.quantity + $5), dollar_cost = (portfolio.dollar_cost + $3), avg_price = ((portfolio.dollar_cost + $3) / (portfolio.quantity + $5)) WHERE portfolio.coin_id = $2 AND portfolio.trader_id = $1;`, [user_id, transaction.coin_id, transaction.total_trade_fiat, transaction.purchase_price, transaction.total_trade_coin]);
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
 
-  return 'Hit Fulfill Buy Transaction';
+    // ADD INITIAL PORTION OF TRANSACTION HERE
+
+    // Subtract total_trade_fiat and update dollar cost for CASH
+    query(`UPDATE portfolio SET quantity = (quantity - $1), dollar_cost = (dollar_cost - $1) WHERE coin_id = (SELECT id FROM coins WHERE acronym = 'usd') AND trader_id = $2;`, [transaction.total_trade_fiat, user_id]);
+    // add total_trade_coin to portfolio || Dollar Cost || Avg Price
+    query(`INSERT INTO portfolio(trader_id, coin_id, dollar_cost, avg_price, quantity) VALUES($1, $2, $3, $4, $5) ON CONFLICT (trader_id, coin_id) DO UPDATE SET quantity = (portfolio.quantity + $5), dollar_cost = (portfolio.dollar_cost + $3), avg_price = ((portfolio.dollar_cost + $3) / (portfolio.quantity + $5)) WHERE portfolio.coin_id = $2 AND portfolio.trader_id = $1;`, [user_id, transaction.coin_id, transaction.total_trade_fiat, transaction.purchase_price, transaction.total_trade_coin]);
+    await client.query('COMMIT');
+    return 'Buy Transaction Completed';
+  } catch (err) {
+    await client.query('ROLLBACK');
+    console.error(err);
+  } finally {
+    client.release();
+  }
 }
 
 exports.fulfillSellTransaction = (transaction) => {
-  // add total_trade_fiat from cash
-  query(`UPDATE portfolio SET quantity = (quantity + $1), dollar_cost = (dollar_cost + $1) WHERE coin_id = (SELECT id FROM coins WHERE acronym = 'usd') AND trader_id = $2;`, [transaction.total_trade_fiat, user_id]);
-  // subtract total_trade_coin to portfolio || Dollar Cost || Avg Price
-  query(`UPDATE portfolio SET quantity = (quantity - $1), dollar_cost = (dollar_cost - ($1 * $2)), avg_price = (dollar_cost - ($1 * $2)) / (quantity - $1) WHERE coin_id = $3 AND trader_id = 1;`, [transaction.total_trade_coin, transaction.purchase_price, transaction.coin_id, user_id]);
-  return 'Hit Fulfill Sell Transaction';
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+
+    // ADD INITIAL PORTION OF TRANSACTION HERE
+
+    // add total_trade_fiat from cash
+    query(`UPDATE portfolio SET quantity = (quantity + $1), dollar_cost = (dollar_cost + $1) WHERE coin_id = (SELECT id FROM coins WHERE acronym = 'usd') AND trader_id = $2;`, [transaction.total_trade_fiat, user_id]);
+    // subtract total_trade_coin to portfolio || Dollar Cost || Avg Price
+    query(`UPDATE portfolio SET quantity = (quantity - $1), dollar_cost = (dollar_cost - ($1 * $2)), avg_price = (dollar_cost - ($1 * $2)) / (quantity - $1) WHERE coin_id = $3 AND trader_id = 1;`, [transaction.total_trade_coin, transaction.purchase_price, transaction.coin_id, user_id]);
+    await client.query('COMMIT');
+    return 'Hit Fulfill Sell Transaction';
+  } catch (err) {
+    await client.query('ROLLBACK');
+    console.error(err);
+  } finally {
+    client.release();
+  }
 }
