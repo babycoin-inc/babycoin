d3f29e1a8de0eba9f4893015eb1f8c49a8aa7ab2
Morgan Harrison <morganharrison13@gmail.com>
2022-12-12 22:31:14 -0700

    Uncommented out newsfeed route

--- server/index.js
+++ server/index.js
@@ -30,18 +30,18 @@ app.get('/users/:id/transactions/', home.getTransactions);
 app.delete('/users/:id/transactions/', home.clearTransactions);
 app.delete('/users/:id/portfolio/', home.clearPortfolio)
 
-// app.get("/newsfeed", async (req, res) => {
-//   console.log(req.body);
-//   try {
-//     const result = await nf.getNews(n=10);
-//     if(result.length > 0) {
-//       res.status(200).send(result);
-//     }
-//   } catch (err) {
-//       res.status(500);
-//       console.log(err);
-//   }
-// });
+app.get("/newsfeed", async (req, res) => {
+  console.log(req.body);
+  try {
+    const result = await nf.getNews(n=10);
+    if(result.length > 0) {
+      res.status(200).send(result);
+    }
+  } catch (err) {
+      res.status(500);
+      console.log(err);
+  }
+});
 
 app.get("/nfAPI", (req, res) => {
   nf.runAPI((err,result) => {
@@ -53,14 +53,10 @@ app.get("/nfAPI", (req, res) => {
   })
 })
 
-<<<<<<< HEAD
-=======
 app.get('/coins/markets', market.getCoins);
 
 app.get('/leaderboard', leaderboard.getLeaderboard);
 
-
->>>>>>> main
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
