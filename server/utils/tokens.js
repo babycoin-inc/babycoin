4c3f9798fae3e8cb4e63c6bf29732eff10bb0079
Nick Hays <nicholasahays@gmail.com>
2022-12-12 20:51:13 -0700

    Adds createToken function

--- server/utils/tokens.js
+++ server/utils/tokens.js
@@ -3,16 +3,26 @@ const jwt = require('jsonwebtoken');
 const { Auth } = require('../../../models/models.js');
 const { Login } = Auth;
 
-const verifyToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if(token === null) return res.sendStatus(401);
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if(err) return res.sendStatus(403);
-    req.username = decoded.username;
-    next();
-  })
+const createToken = (id, username) => {
+  const token = jwt.sign(
+    { userid: id, username: username },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: '15m' }
+  );
+  return token;
 }
+
+// const verifyToken = (req, res, next) => {
+//   const authHeader = req.headers['authorization'];
+//   const token = authHeader && authHeader.split(' ')[1];
+//   if(token === null) return res.sendStatus(401);
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if(err) return res.sendStatus(403);
+//     req.username = decoded.username;
+//     next();
+//   })
+// }
+
 
 
 // const refreshToken = async(req, res) => {
@@ -36,4 +46,4 @@ const verifyToken = (req, res, next) => {
 
 // module.exports = refreshToken;
 
-module.exports = verifyToken;
+module.exports = createToken;
