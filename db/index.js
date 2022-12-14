f8128a361aaa3dd436f98a63ae1a74e8c2ffc41f
Alex Yeung <alexyeung@Rogelios-MacBook-Pro.local>
2022-12-14 13:53:32 +0900

    Updated

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { Pool, Client } = require('pg');
 const fs = require('fs');
+
 const pool = new Pool({
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
@@ -18,19 +19,14 @@ const sql = fs.readFileSync('db/init.sql', 'utf8');
     pool.query(sql);
   } catch(err) {
     if (err.code === '3D000') {
-      try {
-        console.log(`Database 'babycoin' doesn't exist. Creating database 'babycoin'.`)
-        const client = new Client({ database: process.env.PGUSER });
-        await client.connect();
-        await client.query('CREATE DATABASE babycoin');
-        client.end();
-        const pool = new Pool();
-        console.log(`Connected to database ${process.env.PGDATABASE} on port ${process.env.PGPORT}`);
-        await pool.query(sql);
-        await pool.query(`COPY achievements FROM '${__dirname}/achievements.csv' DELIMITER ',' CSV HEADER`);
-      } catch (err) {
-        console.log('Error in the database:', err);
-      }
+      console.log(`Database 'babycoin' doesn't exist. Creating database 'babycoin'.`)
+      const client = new Client({ database: process.env.PGUSER });
+      await client.connect();
+      await client.query('CREATE DATABASE babycoin');
+      client.end();
+      const pool = new Pool();
+      console.log(`Connected to database ${process.env.PGDATABASE} on port ${process.env.PGPORT}`);
+      pool.query(sql);
     }
   }
 })();
