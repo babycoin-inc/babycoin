978c5b404a535310825f2d61508d1ccb2628d569
Nick Hays <nicholasahays@gmail.com>
2022-12-13 23:23:55 -0700

    Adds createRefreshToken to login controller and puts refreshToken in payload

--- server/controllers/Auth/index.js
+++ server/controllers/Auth/index.js
@@ -1,7 +1,7 @@
 const { Auth } = require('../../../models/models.js');
 const { isUsernameUnavailable, registerUser } = Auth;
 const { hashPassword } = require('../../utils/passwords.js');
-const createToken = require('../../utils/tokens.js');
+const { createAccessToken, createRefreshToken } = require('../../utils/tokens.js');
 
 const AuthControllers = {
   signupController: async (req, res) => {
@@ -20,8 +20,9 @@ const AuthControllers = {
   loginController: (req, res) => {
     console.log('REQ.USER', req.user)
     const { id, username } = req.user;
-    const token = createToken(id, username);
-    const authorizedUser = { id, username, token };
+    const accessToken = createAccessToken(id, username);
+    const refreshToken = createRefreshToken(id, username);
+    const authorizedUser = { id, username, accessToken, refreshToken };
     res.status(200).send(authorizedUser);
   }
 }
