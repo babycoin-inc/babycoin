aa79171f9d5e96a1927579c9c0e7c6d06d1fa778
Dongning Song <dongningsong@dongnings-mini.lan>
2022-12-14 02:11:55 -0700

    refactor1

---
+++ client/src/components/MarketWatch/MarketWatch.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const MarketWatch = ({coins, handleCoinClick}) => {
+  return (
+        <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+        <table className="w-full text-center">
+          <thead className="border-b bg-zinc-700">
+            <tr>
+              <th scope="col" className="text-lg font-medium text-white px-3 py-6">
+                #
+              </th>
+              <th scope="col" className="text-lg font-medium text-white px-3 py-6">
+                Coin
+              </th>
+              <th scope="col" className="text-lg font-medium text-white px-3 py-6">
+                Price
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+              {coins.map((coin, index) => (
+                <tr className="border-b bg-zinc-900 boder-gray-900 hover:bg-zinc-500">
+                  <td className="text-sm text-white font-light px-2 py-5 whitespace-nowrap">{coin.id}</td>
+                  <td className="text-sm text-white font-light px-2 py-5 whitespace-nowrap" onClick={handleCoinClick}>{coin.name}</td>
+                  <td className="text-sm text-white font-light px-2 py-5 whitespace-nowrap">{coin.latest_price}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+    </div>
+  );
+}
+
+export default MarketWatch;
