df517196cdf817a6689a6746beab87df63c69e60
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-12-15 09:35:33 -0600

    revise deployment files

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,9 +1,6 @@
 PATH="/home/ec2-user/.nvm/versions/node/v14.21.1/bin"
 
 cd /home/ec2-user/app/
-echo "Installing app dependencies..."
 npm ci
-echo "Building Tailwind CSS..."
 npm build-css-prod
-echo "Building webpack files"
 npm build
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,2 @@
-PATH="/home/ec2-user/.nvm/versions/node/v14.21.1/bin"
-
 cd /home/ec2-user/app/
 npm start
