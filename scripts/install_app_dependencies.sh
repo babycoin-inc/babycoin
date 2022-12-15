4bcaffd7243fb2e1608a03f0ed5f1b89b2078af6
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-12-15 08:35:21 -0600

    revise deployment files

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && .
+
 cd /home/ec2-user/app/
 npm ci
 npm build-css-prod
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && .
+
 cd /home/ec2-user/app/
 npm start
