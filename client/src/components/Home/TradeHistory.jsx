2fbdef08e93f9ac999c7dc3e30137d0ac110073b
Duke Romkey <dukeromkey@gmail.com>
2022-12-14 21:31:54 -0600

    Working on user instructions

--- client/src/components/Home/TradeHistory.jsx
+++ client/src/components/Home/TradeHistory.jsx
@@ -11,38 +11,53 @@ function TradeHistory({ tradeHistory }) {
     }
   };
 
+  let hasMadeTrades = false;
+
+  if (tradeHistory.length >= 1) {
+    hasMadeTrades = true;
+  }
+
+  console.log('hasMadeTrades', hasMadeTrades);
+
   return (
     <div className="mt-6">
       <div className="bg-zinc-700 rounded-t-xl pt-2.5 px-5 w-fit">
         <h2>Trade History</h2>
       </div>
       <div className="bg-zinc-700 rounded-b-xl rounded-tr-xl px-3 py-5">
-        <div className="max-h-96 overflow-auto">
-          <table className="w-full mx-auto table-auto">
-            <thead className="bg-zinc-600 sticky top-0">
-              <tr>
-                <th className="p-6"></th>
-                <th>Name</th>
-                <th>Amount</th>
-                <th>Price</th>
-                <th>Type</th>
-                <th>Date</th>
-              </tr>
-            </thead>
-            <tbody className="bg-zinc-500">
-              {tradeHistory.map((trade, index) => (
-                <tr key={index} className="border-t border-zinc-600  hover:bg-zinc-400">
-                  <td className="py-2"><img src={trade.image} className="h-10 mx-auto" /></td>
-                  <td className="text-center">{trade.name}<i className="block text-sm">{trade.acronym.toUpperCase()}</i></td>
-                  <td className="text-center">${Number(trade.total_trade_fiat).toFixed(2)}<i className="block text-sm">.{`${trade.total_trade_coin} ${trade.acronym.toUpperCase()}`}</i></td>
-                  <td className="text-center">${Number(trade.purchase_price).toLocaleString("en-US")}</td>
-                  {orderTypeColor(trade.order_type)}
-                  <td className="text-center">{dateFormat(trade.order_datetime, 'mmm d')}</td>
+        {!hasMadeTrades && (
+          <div>
+            <img alt="Dollar Ethereum Exchange icon" srcset="https://img.icons8.com/ios-filled/512/exchange-money-ethereum.png" style={{width: '100px', height: '100px', filter: 'invert(50%) sepia(100%) saturate(346%) hue-rotate(111deg) brightness(93%) contrast(100%)'}} />
+          </div>
+        )}
+        {hasMadeTrades && (
+          <div className="max-h-96 overflow-auto">
+            <table className="w-full mx-auto table-auto">
+              <thead className="bg-zinc-600 sticky top-0">
+                <tr>
+                  <th className="p-6"></th>
+                  <th>Name</th>
+                  <th>Amount</th>
+                  <th>Price</th>
+                  <th>Type</th>
+                  <th>Date</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody className="bg-zinc-500">
+                {tradeHistory.map((trade, index) => (
+                  <tr key={index} className="border-t border-zinc-600  hover:bg-zinc-400">
+                    <td className="py-2"><img src={trade.image} className="h-10 mx-auto" /></td>
+                    <td className="text-center">{trade.name}<i className="block text-sm">{trade.acronym.toUpperCase()}</i></td>
+                    <td className="text-center">${Number(trade.total_trade_fiat).toFixed(2)}<i className="block text-sm">.{`${trade.total_trade_coin} ${trade.acronym.toUpperCase()}`}</i></td>
+                    <td className="text-center">${Number(trade.purchase_price).toLocaleString("en-US")}</td>
+                    {orderTypeColor(trade.order_type)}
+                    <td className="text-center">{dateFormat(trade.order_datetime, 'mmm d')}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
     </div>
   )
