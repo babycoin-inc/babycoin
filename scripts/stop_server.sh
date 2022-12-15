a955f7e6388b4d32a49aa9102337aa3e262ef241
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-12-15 11:17:27 -0600

    small deployment fixes

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads 
 
-cd /home/ec2-user/app/
-npm start &
+# Stop the npm start process
+npx kill-port 3000
