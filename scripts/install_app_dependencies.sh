1d398ee68580035023fe80349ec1399c4fed8ad7
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-12-15 09:56:15 -0600

    revise deployment files

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads 
 
+sudo chmod -R 777 /home/ec2-user/app/
 cd /home/ec2-user/app/
 npm ci
 npm run build-css-prod
 npm run build
+sudo chmod -R 775 /home/ec2-user/app/
