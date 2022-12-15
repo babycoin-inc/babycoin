50d1c3f0c9593775453aa21e0b9eeab69535cae4
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-12-15 09:40:02 -0600

    revise deployment files

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,4 +1,6 @@
-PATH="/home/ec2-user/.nvm/versions/node/v14.21.1/bin"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads 
 
 cd /home/ec2-user/app/
 npm ci
