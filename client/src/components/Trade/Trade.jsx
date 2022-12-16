607060e092287a85c01bd5d305761107c18e72f5
Morgan Harrison <morganharrison13@gmail.com>
2022-12-16 14:41:31 -0700

    User can sell all of a coin. Page refreshes after order

--- client/src/components/Trade/Trade.jsx
+++ client/src/components/Trade/Trade.jsx
@@ -3,7 +3,7 @@ import Order from './Order.jsx';
 import Newsfeed from '../Newsfeeds/newsfeed.jsx'
 import Graph from './Graph.jsx';
 
-function Trade({authenticatedUser, portfolio, coins}) {
+function Trade({authenticatedUser, portfolio, coins, getPortfolioData}) {
 
   return (
     //first element needs to be flex in order to organize containers on trade page
@@ -12,7 +12,7 @@ function Trade({authenticatedUser, portfolio, coins}) {
         <div className="flex w-3/5 p-5">
           <Graph symbol={"BTC"}/>
         </div>
-        <Order authenticatedUser={authenticatedUser} portfolio={portfolio} coins={coins}/>
+        <Order authenticatedUser={authenticatedUser} portfolio={portfolio} coins={coins} getPortfolioData={getPortfolioData}/>
       </div>
       <Newsfeed/>
     </div>
