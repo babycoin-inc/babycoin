e1cb1c0e7465ddf55cfd1a1a9efae58e392abc9a
Nick Hays <nicholasahays@gmail.com>
2022-12-16 16:43:35 -0700

    Adds useEffects to get value out of local storage, currently storing user id, attempting to store session

--- client/src/components/Page.jsx
+++ client/src/components/Page.jsx
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import App from './App.jsx';
 import Login from './Login/Login.jsx';
 
 const Page = () => {
-  const [authorizedUser, setAuthorizedUser] = useState(null);
+  const [authorizedUser, setAuthorizedUser] = useState(JSON.parse(window.sessionStorage.getItem('userID')));
 
   // return (
   //   {authorizedUser ? <App /> : <Login updateUser={setAuthorizedUser}/>}
   // )
+
+  // useEffect(() => {
+  //   setAuthorizedUser(JSON.parse(window.localStorage.getItem('userID')));
+  // }, []);
+
+  useEffect(() => {
+    window.sessionStorage.setItem('userID', JSON.stringify(authorizedUser));
+  }, [authorizedUser]);
 
   if(!authorizedUser) {
     return <Login updateUser={setAuthorizedUser}/>
