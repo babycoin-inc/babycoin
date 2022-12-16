dad395c8974135e4ee189a69a25252633aad8eba
Morgan Harrison <morganharrison13@gmail.com>
2022-12-15 23:46:24 -0700

    Messages are rendering correctly, user can only sell coins they have in portfolio

--- client/src/components/Trade/Order.jsx
+++ client/src/components/Trade/Order.jsx
@@ -9,11 +9,11 @@ import { NumericFormat } from 'react-number-format';
 function Order({ authenticatedUser, portfolio, coins }) {
   const [orderType, setOrderType] = useState("buy");
   let [orderAmount, setOrderAmount] = useState("");
-  let [orderUnits, setOrderUnits] = useState('USD');
+  let [orderUnits, setOrderUnits] = useState('usd');
   //DEFAULT COIN: CHANGE TO SELECTED COIN
   let [coin, setCoin] = useState(coins[2]);
-  let [total_trade_coin, setTotalTradeCoin] = useState(0);
-  let [total_trade_fiat, setTotalTradeFiat] = useState(0);
+  let [total_trade_coin, setTotalTradeCoin] = useState();
+  let [total_trade_fiat, setTotalTradeFiat] = useState();
 
   console.log('coins: ', coins);
   console.log('portfolio: ', portfolio);
@@ -21,31 +21,114 @@ function Order({ authenticatedUser, portfolio, coins }) {
   console.log('total_trade_fiat: ', total_trade_fiat);
   console.log('orderAmount: ', orderAmount);
 
-
-  let view = orderType === 'buy' ? <Buy /> : <Sell />;
   let buyButton;
   let sellButton;
   let orderInput;
   let sellAll;
+  let convertButton;
+  let maxOrderAmountMessage;
+  let coinOrderList;
+  const getCoinsInPortfolio = () => {
+    let result = [];
+    portfolio.forEach((asset) => {
+      console.log('porftolio to start: ', portfolio);
+      if (asset.acronym !== 'usd') {
+        result.push(asset.coin);
+      }
+    });
+    return result;
+  }
 
   if (orderType === 'buy') {
-    buyButton = <button disabled={true} className="basis-1/2 border-2 text-xl bg-zinc-800 text-orange-500 font-semibold border border-orange-500 rounded py-2 px-5 mx-auto">Buy</button>
-    sellButton = <button onClick={() => setOrderType('sell')} className="basis-1/2 border-2 text-xl bg-orange-400 text-orange-900 font-semibold border border-orange-500 rounded py-2 px-5 mx-auto active:border active:border-orange-400 hover:bg-orange-500">Sell</button>
+    coinOrderList =  coins.slice(1).map((crypto, index) => {
+      return <option value={crypto.name} key={index}>{crypto.name}</option>
+    })
 
+    buyButton = <button disabled={true} className="basis-1/2 border-2 text-xl bg-zinc-800 text-orange-500 font-semibold border border-orange-500 rounded py-2 px-5 mx-auto">Buy</button>
+
+    if (portfolio.length <= 1) {
+      sellButton = <button disabled onClick={() => setOrderType('sell')} className="basis-1/2 border-2 text-xl bg-orange-400 text-orange-900 font-semibold border border-orange-500 rounded py-2 px-5 mx-auto active:border active:border-orange-400 hover:bg-orange-500">Sell</button>
+    } else {
+      sellButton = <button onClick={() => {
+        //if current selected coin is not in the user's portfolio, reset coin to first coin in portfolio
+        const coinsInPortfolio = getCoinsInPortfolio();
+        console.log('coinsInPortfolio: ', coinsInPortfolio);
+        console.log('coins.name: ',coin.name);
+        if (coinsInPortfolio.indexOf(coin.name) === -1) {
+          var coinToSet;
+          for (var i = 0; i < coins.length; i++) {
+            if (coins[i].name === coinsInPortfolio[0]) {
+              console.log('MADE IT');
+              console.log('coins[i].name: ',coins[i].name);
+              console.log('coinsInPortfolio: ', coinsInPortfolio);
+              coinToSet = coins[i];
+              break;
+            }
+          }
+          console.log('coinToSet: ', coinToSet);
+          setCoin(coinToSet);
+        }
+        setOrderType('sell')}
+      }
+        className="basis-1/2 border-2 text-xl bg-orange-400 text-orange-900 font-semibold border border-orange-500 rounded py-2 px-5 mx-auto active:border active:border-orange-400 hover:bg-orange-500">Sell</button>
+    }
   } else if (orderType === 'sell') {
     buyButton = <button onClick={() => setOrderType('buy')} className="basis-1/2 border-2 text-xl bg-orange-400 text-orange-900 font-semibold border border-orange-500 rounded py-2 px-5 mx-auto active:border active:border-orange-400 hover:bg-orange-500">Buy</button>
     sellButton = <button disabled={true} className="basis-1/2 border-2 text-xl bg-zinc-800 text-orange-500 font-semibold border border-orange-500 rounded py-2 px-5 mx-auto">Sell</button>
 
     sellAll =
-    <div className="text-center bg-zinc-400 rounded-xl hover:bg-zinc-500 py-2 px-10 mx-auto relative left-5">
-      <button onClick={() => setOrderAmount(orderUnits === 'USD' ? total_trade_fiat : total_trade_coin)}>Sell All</button>
-    </div>
+      <div className="text-center bg-zinc-400 rounded-xl hover:bg-zinc-500 py-2 px-10 mx-auto relative left-5">
+        <button onClick={() => setOrderAmount(orderUnits === 'usd' ? total_trade_fiat : total_trade_coin)}>Sell All</button>
+      </div>
+
+    coinOrderList = portfolio.map((asset, index) => {
+      //only return coins we have assets for in portfolio, and excluding usd
+      if (asset.acronym !== 'usd') {
+        return <option value={asset.coin} key={index}>{asset.coin}</option>
+      }
+    })
   }
 
-  if (orderUnits === 'USD') {
+  if (orderUnits === 'usd') {
     orderInput = <NumericFormat className="h-14 text-xl text-center bg-zinc-400 rounded-xl hover:bg-zinc-500" displayType="input" type="text" value={orderAmount} allowNegative={false} valueIsNumericString={true} defaultValue="Order Amount" prefix='$' decimalScale={2} allowLeadingZeros={false} thousandSeparator="," onValueChange={(values, sourceInfor) => { handleOrderAmountChange(values.value) }} />
+
+    console.log('undefined coin.acronym: ', coin, coin.acronym);
+    convertButton = <div className="self-start text-sm text-center">{coin.acronym.toUpperCase()}</div>
+
   } else if (orderUnits === 'coin') {
     orderInput = <NumericFormat className="h-14 text-xl text-center bg-zinc-400 rounded-xl hover:bg-zinc-500" displayType="input" type="text" value={orderAmount} allowNegative={false} valueIsNumericString={true} defaultValue="Order Amount" decimalScale={5} onValueChange={(values, sourceInfor) => { handleOrderAmountChange(values.value) }} />
+
+    convertButton = <div className="self-start text-m text-center">$</div>
+  }
+
+  const roundNumUpToDigit = (num, digits) => {
+    const numAsString = num.toString();
+    const indexOfDec = numAsString.indexOf('.');
+    if (indexOfDec === -1) {
+      return num;
+    }
+    let right = numAsString.slice(indexOfDec + 1, indexOfDec + 1 + digits);
+    let left = numAsString.slice(-numAsString.length, indexOfDec + 1);
+    return Number(left + right);
+  }
+
+  const getAssetFromPortfolio = (acronym) => {
+    const index = portfolio.findIndex((asset, index) => {return asset.acronym === acronym});
+    return portfolio[index];
+  }
+
+  if (orderUnits === 'coin' && orderType === 'buy') {
+    let maxCoinOrderAmount = getAssetFromPortfolio('usd').quantity / coin.latest_price;
+    maxOrderAmountMessage = <div className="text-sm text-center">You can {orderType} up to {roundNumUpToDigit(maxCoinOrderAmount, 5)} {coin.acronym.toUpperCase()}</div>
+  } else if(orderUnits === 'usd' && orderType === 'buy') {
+    maxOrderAmountMessage = <div className="text-sm text-center">You can {orderType} up to ${getAssetFromPortfolio('usd').quantity}</div>
+  } else if(orderUnits === 'coin' && orderType === 'sell') {
+    maxOrderAmountMessage = <div className="text-sm text-center">You can {orderType} up to {roundNumUpToDigit((getAssetFromPortfolio(coin.acronym)).quantity, 5)} {coin.acronym.toUpperCase()}</div>
+  } else if(orderUnits === 'usd' && orderType === 'sell') {
+    console.log('coin selling: ', coin);
+    console.log('getAssetFromPortfolio(coin.acronym): ', getAssetFromPortfolio(coin.acronym));
+    console.log('getAssetFromPortfolio(coin.acronym).quantity: ', getAssetFromPortfolio(coin.acronym).quantity);
+    maxOrderAmountMessage = <div className="text-sm text-center">You can {orderType} up to ${roundNumUpToDigit(getAssetFromPortfolio(coin.acronym).quantity * coin.latest_price, 2)}</div>
   }
 
 const handleOrderAmountChange = (value) => {
@@ -61,7 +144,7 @@ const calculateTotals = (value) => {
   let total_fiat;
   let total_coin;
 
-  if (orderUnits === 'USD') {
+  if (orderUnits === 'usd') {
     total_fiat = parseFloat(value);
     total_coin = parseFloat(value) / coin.latest_price;
     console.log('total_coin: ', total_coin);
@@ -69,6 +152,7 @@ const calculateTotals = (value) => {
     setTotalTradeFiat(total_fiat);
     setTotalTradeCoin(total_coin);
   } else if (orderUnits === 'coin') {
+    //round down value at the fifth digit place
     total_fiat = parseFloat(value) * coin.latest_price;
     total_coin = parseFloat(value);
     setTotalTradeFiat(total_fiat);
@@ -91,7 +175,7 @@ const submitOrder = async () => {
   try {
     const orderResult = await axios.post(`/users/${authenticatedUser}/transactions/${orderType}`, {
       coin_id: coin.id,
-      currency: 'USD',
+      currency: 'usd',
       purchase_price: coin.latest_price,
       'total_trade_coin': total_trade_coin,
       'total_trade_fiat': total_trade_fiat,
@@ -106,12 +190,12 @@ const submitOrder = async () => {
 }
 
 const handleUnitsConversion = () => {
-  if (orderUnits === 'USD') {
+  if (orderUnits === 'usd') {
     setOrderUnits('coin');
     //take existing order amount and convert it to coin quantity
     setOrderAmount(total_trade_coin);
   } else if (orderUnits === 'coin') {
-    setOrderUnits('USD');
+    setOrderUnits('usd');
     //take existing order amount and convert it to coin quantity
     setOrderAmount(total_trade_fiat);
   }
@@ -130,12 +214,12 @@ return (
             <HiOutlineSwitchVertical />
           </IconContext.Provider>
         </button>
-        <div className="self-start text-sm text-center">{coin.acronym.toUpperCase()}</div>
+        {convertButton}
       </div>
       <div className="">
         {console.log('orderAmount in return: ', orderAmount)}
         {orderInput}
-        <div className="text-sm text-center">You can {orderType} up to $500.00</div>
+        {maxOrderAmountMessage}
       </div>
     </div>
     {sellAll}
@@ -146,9 +230,7 @@ return (
         setCoin(coins[coinIndex]);
         handleOrderAmountChange("");
       }}>
-        {coins.slice(1).map((crypto, index) => {
-          return <option value={crypto.name} key={index}>{crypto.name}</option>
-        })}
+       {coinOrderList}
       </select>
     </div>
     <div className="flex justify-between gap-4">
