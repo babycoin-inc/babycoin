8b0fea1a39ad8719cc59ae604fc0d73a3cbfa8a3
Nick Hays <nicholasahays@gmail.com>
2022-12-17 12:20:47 -0700

    Trying to fix see through form

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -88,12 +88,12 @@ const Login = ({ updateUser }) => {
 
   return (
     <div className='relative w-full max-h-max'>
-      <img className='absolute w-full h-full object-cover mix-blend-overlay min-h-screen' src={bgImage} alt="bgImage" />
+      <img className='absolute w-full h-full mix-blend-overlay object-cover min-h-screen' src={bgImage} alt="bgImage" />
       <div className='flex justify-center items-center py-8'>
         <img className= 'w-48 h-48' src={babycoin} alt='babycoin'/>
       </div>
       <div className='flex justify-center items-center py-8'>
-        <form className='max-w-[400px] w-full mx-auto opacity-100 bg-white p-8 rounded-xl'>
+        <form className='max-w-[400px] w-full mx-auto  p-8 rounded-xl bg-white mix-blend-normal'>
           <h2 className='text-4xl font-bold text-center py-4'>BabyCoin</h2>
           <div className='flex justify-center py-8'>
             <button className='border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center rounded-xl' onClick={handleGoogleLogin}> <FcGoogle className='mr-2' />Google</button>
