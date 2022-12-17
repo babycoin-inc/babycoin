fe1f91ffd5d34c43e74132a4780b615ab56b1752
dchiu91 <d.chiu91@yahoo.com>
2022-12-16 18:34:37 -0800

    merged index.js

--- server/index.js
+++ server/index.js
@@ -1,28 +1,42 @@
 require('../db/index.js'); //tests db connection
-require("dotenv").config();
-
+require('../passport.config.js');
 const express = require('express');
+require('dotenv').config();
+const passport = require('passport');
 
 const app = express();
-
 const port = process.env.PORT || 4000;
 
-const { nf, home, trade, leaderboard, market, achievements} = require('./controllers/controllers.js');
+const flash = require('express-flash');
+const session = require('cookie-session');
 
 const cron = require('node-cron');
+
+/**Controllers */
+const { auth, nf, home, trade, leaderboard, market, achievements} = require('./controllers/controllers.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  name: 'session',
+  secret: 'secret',
+  expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+}));
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.post('/signup', auth.signupController);
+app.post('/login', passport.authenticate('local'), auth.loginController);
 
 app.get('/coins', trade.getCoin);
 
 app.post('/users/:id/transactions/buy', trade.insertBuyTransaction);
 app.post('/users/:id/transactions/sell', trade.insertSellTransaction);
 
+app.get('/users/:id/achievements', achievements.getUserAchievements);
+app.post('/users/:id/achievements/:achievement', achievements.addUserAchievement);
 app.get('/achievements', achievements.getAchievements);
-app.get('/achievements/:trader_id', achievements.getUserAchievements);
-app.post('/achievements/:trader_id', achievements.addUserAchievement);
 
 // Gets all assets in user portfolio
 app.get('/users/:id/balances/', home.getPortfolioAssets);
@@ -32,28 +46,8 @@ app.get('/users/:id/transactions/', home.getTransactions);
 // Resets Portfolio, transaction history, and adds starting cash and achievement
 app.delete('/users/:id/portfolio/', home.clearPortfolio)
 
-app.get("/newsfeed", async (req, res) => {
-  console.log(req.body);
-  try {
-    const result = await nf.getNews(n=10);
-    if(result.length > 0) {
-      res.status(200).send(result);
-    }
-  } catch (err) {
-      res.status(500);
-      console.log(err);
-  }
-});
-
-app.get("/nfAPI", (req, res) => {
-  nf.runAPI((err,result) => {
-    if(err){
-      res.status(500);
-    } else {
-      res.status(200).send(result);
-    }
-  })
-})
+app.get("/newsfeed/:coin", nf.getNews);
+app.get("/nfAPI", nf.runAPI);
 
 
 cron.schedule('*/30 * * * * *', () => {
@@ -66,6 +60,7 @@ app.get('/coins/markets', market.getCoins);
 
 
 app.get('/leaderboard', leaderboard.getLeaderboard);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
