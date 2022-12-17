ccc8a11a95b737e4da20d315c2d7b548522e093f
Nick Hays <nicholasahays@gmail.com>
2022-12-16 22:59:18 -0700

    Adds req to /getuser to get google cookie

--- client/src/components/Page.jsx
+++ client/src/components/Page.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import App from './App.jsx';
 import Login from './Login/Login.jsx';
+import axios from 'axios';
 
 const Page = () => {
   const [authorizedUser, setAuthorizedUser] = useState(JSON.parse(window.sessionStorage.getItem('userID')));
@@ -14,13 +15,23 @@ const Page = () => {
   // }, []);
 
   useEffect(() => {
+    axios
+      .get('/getuser', { withCredentials: true })
+      .then(result => {
+        if (result.data) setAuthorizedUser(result.data);
+        console.log('RESULT DATA FROM GETUSER CALL', result.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  useEffect(() => {
     window.sessionStorage.setItem('userID', JSON.stringify(authorizedUser));
   }, [authorizedUser]);
 
   if(!authorizedUser) {
     return <Login updateUser={setAuthorizedUser} updateSession={setSessionCookie}/>
   } else {
-    return <App className = "bg-zinc-900 pb-8"/>
+    return <App className = "bg-zinc-900 pb-8" setAuthorizedUser={setAuthorizedUser}/>
   }
 }
 
