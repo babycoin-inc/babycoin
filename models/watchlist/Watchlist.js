ce31cb29a0d1c2c590831b8a2122658220c07cc1
Dongning Song <dongningsong@dongnings-mini.lan>
2022-12-17 05:55:13 -0700

    refactoring the query

--- models/watchlist/Watchlist.js
+++ models/watchlist/Watchlist.js
@@ -15,15 +15,23 @@ exports.deleteCoinsFromWatchlist = async (userId, deletedCoin) => {
   //   }
   // })
 
-  for await (const coin of arr) {
-    if (coin === deletedCoin) {
-      const text1 = 'UPDATE trader_watchlist SET watchlist = ( SELECT array_remove(watchlist, $1) FROM trader_watchlist WHERE trader_id = $2) WHERE trader_id = $2';
-      const params1 = [deletedCoin, userId];
-      await query(text1, params1);
+  async function checkAndDelete () {
+    for (const coin of arr) {
+      if (coin === deletedCoin) {
+        const text1 = 'UPDATE trader_watchlist SET watchlist = ( SELECT array_remove(watchlist, $1) FROM trader_watchlist WHERE trader_id = $2) WHERE trader_id = $2';
+        const params1 = [deletedCoin, userId];
+        await query(text1, params1);
+      }
     }
   }
 
-  result = await query(text0, params0);
+  checkAndDelete()
+  .then(result => {
+    console.log(result);
+  })
+  .catcj(err => console.log('errr'));
+
+  // result = await query(text0, params0);
   console.log('hhhhh123');
   return result['rows'];
 
--- server/controllers/Watchlist/watchlist.js
+++ server/controllers/Watchlist/watchlist.js
@@ -3,7 +3,7 @@ const watchlist = require('../../../models/watchlist/Watchlist.js');
 exports.removeFromWatchlist = (req, res) => {
   watchlist.deleteCoinsFromWatchlist(req.params['id'], req.params['coin'])
   .then(result => {
-    console.log('result', result[0]['watchlist']);
+    console.log('result!!!', result[0]['watchlist']);
     res.send(result[0]['watchlist']);
   })
   .catch(err => res.send(err));
