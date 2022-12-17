08f62435e537d96bec32aacc14c80b6c9979054f
Dongning Song <dongningsong@dongnings-mini.lan>
2022-12-17 05:15:48 -0700

    could remove clicked coins from the database

--- models/watchlist/Watchlist.js
+++ models/watchlist/Watchlist.js
@@ -1,10 +1,21 @@
 const {query} = require('../../db/index');
 
-exports.deleteCoinsFromWatchlist = async (userId, coin) => {
+exports.deleteCoinsFromWatchlist = async (userId, deletedCoin) => {
   const params0 = [userId];
   const text0 = 'SELECT watchlist FROM trader_watchlist WHERE trader_id = $1';
   const result = await query(text0, params0);
 
-  console.log('result.rows', result['rows']);
+  const arr = result['rows'][0]['watchlist'];
+
+  arr.forEach( async (coin, index) => {
+    if (coin === deletedCoin) {
+      const text1 = 'UPDATE trader_watchlist SET watchlist = ( SELECT array_remove(watchlist, $1) FROM trader_watchlist WHERE trader_id = $2) WHERE trader_id = $2';
+      const params1 = [deletedCoin, userId];
+      await query(text1, params1);
+    }
+  })
+
+  result = await query(text0, params0);
   return result['rows'];
+
 }
--- server/controllers/Watchlist/watchlist.js
+++ server/controllers/Watchlist/watchlist.js
@@ -4,7 +4,7 @@ exports.removeFromWatchlist = (req, res) => {
   console.log(req.params); // { id: '1', coin: 'Ethereum' }
   watchlist.deleteCoinsFromWatchlist(req.params['id'], req.params['coin'])
   .then(result => {
-    console.log('result', result);
+    console.log('result', result[0]['watchlist']);
     res.send('haha');
   })
   .catch(err => res.send(err));
