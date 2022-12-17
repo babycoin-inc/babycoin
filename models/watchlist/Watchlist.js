35936df3fa7938de3eb91595fcecdd0147f290e6
Dongning Song <dongningsong@dongnings-mini.lan>
2022-12-17 06:22:04 -0700

    it could send the updated watchlist back to the client

--- models/watchlist/Watchlist.js
+++ models/watchlist/Watchlist.js
@@ -3,39 +3,21 @@ const {query} = require('../../db/index');
 exports.deleteCoinsFromWatchlist = async (userId, deletedCoin) => {
   const params0 = [userId];
   const text0 = 'SELECT watchlist FROM trader_watchlist WHERE trader_id = $1';
-  const result = await query(text0, params0);
+  let result = await query(text0, params0);
 
   const arr = result['rows'][0]['watchlist'];
 
-  // arr.forEach( async (coin, index) => {
-  //   if (coin === deletedCoin) {
-  //     const text1 = 'UPDATE trader_watchlist SET watchlist = ( SELECT array_remove(watchlist, $1) FROM trader_watchlist WHERE trader_id = $2) WHERE trader_id = $2';
-  //     const params1 = [deletedCoin, userId];
-  //     await query(text1, params1);
-  //   }
-  // })
 
-  const promiseArray = arr.map(coin => {
+  for (const coin of arr) {
     if (coin === deletedCoin) {
       const text1 = 'UPDATE trader_watchlist SET watchlist = ( SELECT array_remove(watchlist, $1) FROM trader_watchlist WHERE trader_id = $2) WHERE trader_id = $2';
       const params1 = [deletedCoin, userId];
-      return query(text1, params1);
-    } else {
-      return coin;
+      await query(text1, params1);
     }
-  })
-
-  try {
-    await Promise.all(promiseArray);
-  } catch (err) {
-    console.log('promise.all error');
   }
 
-  try {
-    result = await query(text0, params0);
-  } catch (err) {
-    console.log('promise error');
-  }
-
+  result = await query(text0, params0);
+  console.log('hhhhh123');
   return result['rows'];
+
 }
