6c713b3ac4212161ab482b89376ed127c99bb63e
Nick Hays <nicholasahays@gmail.com>
2022-12-17 11:38:47 -0700

    Checks for empty username and password

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -27,7 +27,10 @@ const Login = ({ updateUser }) => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    axios.post('/auth/login', {
+    if(!username.length || !password.length) {
+      return alert('Username and Password Fields must contain values');
+    } else {
+      axios.post('/auth/login', {
         username: username,
         password: password
       })
@@ -36,6 +39,8 @@ const Login = ({ updateUser }) => {
         updateUser(data.id);
       })
       .catch(err => console.log(err))
+    }
+
   }
 
   const handleGoogleLogin = (e) => {
@@ -57,18 +62,23 @@ const Login = ({ updateUser }) => {
     if(password !== passwordComparison) {
       return alert('Passwords must match');
     }
-    axios.post('/auth/signup', {
-      username: username,
-      password: password
-    })
-    .then(({ data }) => {
-      alert('Account Created')
-      updateUser(data.id);
-    })
-    .catch(err => {
-      alert('Something went wrong...')
-      console.log(err)
-    });
+    if(!username.length || !password.length || !passwordComparison.length) {
+      return alert('Username and Password Fields must contain values');
+    } else {
+      axios.post('/auth/signup', {
+        username: username,
+        password: password
+      })
+      .then(({ data }) => {
+        alert('Account Created')
+        updateUser(data.id);
+      })
+      .catch(err => {
+        alert('Something went wrong...')
+        console.log(err)
+      });
+    }
+
   }
 
   const handleLoginPage = (e) => {
@@ -78,15 +88,15 @@ const Login = ({ updateUser }) => {
 
   return (
     <div className='relative w-full max-h-max'>
-      <img className='absolute w-full h-full object-cover mix-blend-overlay' src={bgImage} alt="bgImage" />
+      <img className='absolute w-full h-full object-cover mix-blend-overlay min-h-screen' src={bgImage} alt="bgImage" />
       <div className='flex justify-center items-center py-8'>
         <img className= 'w-48 h-48' src={babycoin} alt='babycoin'/>
       </div>
       <div className='flex justify-center items-center py-8'>
-        <form className='max-w-[400px] w-full mx-auto bg-white p-8'>
+        <form className='max-w-[400px] w-full mx-auto opacity-100 bg-white p-8 rounded-xl'>
           <h2 className='text-4xl font-bold text-center py-4'>BabyCoin</h2>
           <div className='flex justify-center py-8'>
-            <button className='border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center' onClick={handleGoogleLogin}> <FcGoogle className='mr-2' />Google</button>
+            <button className='border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center rounded-xl' onClick={handleGoogleLogin}> <FcGoogle className='mr-2' />Google</button>
             {/* <a href="localhost:3000/auth/google">Click me</a> */}
           </div>
 
@@ -94,32 +104,33 @@ const Login = ({ updateUser }) => {
             <>
             <div className='flex flex-col mb-4'>
             <label>Username</label>
-            <input id='username' className='border relative bg-gray-100 p-2' type='text' onChange={handleInput}/>
+            <input id='username' className='border relative bg-orange-100 p-2' type='text' onChange={handleInput}/>
           </div>
           <div className='flex flex-col'>
             <label>Password</label>
-            <input id='password' className='border relative bg-gray-100 p-2' type='password' onChange={handleInput}/>
+            <input id='password' className='border relative bg-orange-100 p-2' type='password' onChange={handleInput}/>
           </div>
-          <button className='w-full py-3 mt-8 bg-blue-600 hover:bg-blue-500 relative text-white' onClick={handleLogin}>Log In</button>
-          <button className='w-full py-3 mt-8 bg-blue-600 hover:bg-blue-500 relative text-white' onClick={handleLoginPage}>Not a Member? Sign Up Now!</button>
+          <button className='w-full py-3 mt-8 bg-orange-600 hover:bg-orange-500 relative text-white rounded-xl' onClick={handleLogin}>Log In</button>
+
+          <button className='w-full py-3 mt-8 bg-orange-600 hover:bg-orange-500 relative text-white rounded-xl' onClick={handleLoginPage}>Not a Member? Sign Up Now!</button>
           {/* <button className='text-center mt-8' onClick={()=>setLoginPage(false)}>Not a Member? Sign Up Now!</button> */}
           </>
           ) : (
             <>
             <div className='flex flex-col mb-4'>
             <label>Username</label>
-            <input id='username' className='border relative bg-gray-100 p-2' type='text' onChange={handleInput}/>
+            <input id='username' className='border relative bg-orange-100 p-2' type='text' onChange={handleInput}/>
           </div>
           <div className='flex flex-col'>
             <label>Password</label>
-            <input id='password' className='border relative bg-gray-100 p-2' type='password' onChange={handleInput}/>
+            <input id='password' className='border relative bg-orange-100 p-2' type='password' onChange={handleInput}/>
           </div>
           <div className='flex flex-col'>
             <label>Confirm Password</label>
-            <input id='passwordcomparison' className='border relative bg-gray-100 p-2' type='password' onChange={handleInput}/>
+            <input id='passwordcomparison' className='border relative bg-orange-100 p-2' type='password' onChange={handleInput}/>
           </div>
-          <button className='w-full py-3 mt-8 bg-blue-600 hover:bg-blue-500 relative text-white' onClick={handleSignUp}>Sign Up</button>
-          <button className='w-full py-3 mt-8 bg-blue-600 hover:bg-blue-500 relative text-white' onClick={handleLoginPage}>Already a Member? Log In Now!</button>
+          <button className='w-full py-3 mt-8 bg-orange-600 hover:bg-orange-500 relative text-white rounded-xl' onClick={handleSignUp}>Sign Up</button>
+          <button className='w-full py-3 mt-8 bg-orange-600 hover:bg-orange-500 relative text-white rounded-xl' onClick={handleLoginPage}>Already a Member? Log In Now!</button>
           {/* <p className='text-center mt-8' onClick={()=>setLoginPage(true)}>Already a Member? Log In Now!</p> */}
           </>
           )}
