c340dd546d5edb8516f972e0cf63da6e99ae31a0
Dongning Song <dongningsong@dongnings-mini.lan>
2022-12-17 01:52:37 -0700

    if clicking on the star, it  will toggle all the coins, which is wrong

--- client/src/components/Sidebar/Watchlist.jsx
+++ client/src/components/Sidebar/Watchlist.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
+import validStar from './valid.png';
+import invalidStar from './invalid.png';
 
 const Watchlist = ({ userWatchlist, coins }) => {
-  console.log('userWatchlist', userWatchlist, 'coins', coins);
+
+  const [valid, setValid] = useState(true);
+
   return (
     <div className="overflow-x-auto relative">
       <table className="flex items-center text-center border border-white ml-3 mr-3">
@@ -11,6 +15,13 @@ const Watchlist = ({ userWatchlist, coins }) => {
               <tr>
                 <td>{coinInfo.name}</td>
                 <td>{coinInfo.latest_price}</td>
+                <td className={valid ? "valid-star" : "invalid-star"}>
+                  <img
+                    className="w-1/20 h-1/20"
+                    onClick={() => setValid(prevMode => !prevMode)}
+                    src={valid ? validStar : invalidStar}
+                  />
+                </td>
               </tr>
             : null
           ))}
