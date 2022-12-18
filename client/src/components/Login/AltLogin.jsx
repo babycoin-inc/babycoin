9298f549a84b5d44a3243da312d29bd7dc4601b0
Duke Romkey <dukeromkey@gmail.com>
2022-12-18 16:19:35 -0600

    Button sytling

--- client/src/components/Login/AltLogin.jsx
+++ client/src/components/Login/AltLogin.jsx
@@ -56,9 +56,9 @@ function AltLogin() {
 
   return (
     <div className="h-screen min-w-full max-w-screen-2xl mx-auto bg-[url('https://www.kraken.com/accounts/_next/static/media/pro.d68a5101.svg')] bg-cover bg-right-bottom text-white pt-6">
-      <div className="relative flex justify-between w-4/5 mx-auto z-10">
+      <div className="relative flex justify-between w-4/5 mx-auto z-10 items-center">
         <img src={babycoin} className="h-16" />
-        <button>Sign In</button>
+        <button className="rounded-3xl h-fit border-2 py-2 px-4">Sign In</button>
       </div>
       <div className="relative text-zinc-900 z-10 top-20 left-48 w-2/5">
         <h1 className="tracking-widest text-5xl text-center">Create Your Account</h1>
@@ -77,7 +77,7 @@ function AltLogin() {
               <input id="passwordcomparison" className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' type='password' placeholder=" " onChange={handleInput}/>
               <label for="passwordcomparison" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm Password</label>
             </div>
-            <button className='rounded-xl p-3 mt-8 bg-blue-600 hover:bg-blue-500 text-white w-fit self-end' onClick={handleSignUp}>Create Account</button>
+            <button className='rounded-3xl py-3 px-8 mt-8 bg-blue-600 hover:bg-blue-500 text-white w-fit self-end' onClick={handleSignUp}>Create Account</button>
           </form>
       </div>
       <div className="fixed w-3/5 h-full bg-white top-0 left-20">
