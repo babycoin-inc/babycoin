2ed1726eb5f670fc8c4eac357d4a87aaa18ab6cc
Morgan Harrison <morganharrison13@gmail.com>
2022-12-19 21:07:09 -0700

    Created new buy component skeleton

--- client/src/components/Trade/Buy.jsx
+++ client/src/components/Trade/Buy.jsx
@@ -2,10 +2,36 @@ import React, { useState } from 'react';
 
 // import Balance from './Balance.jsx';
 
-function Buy() {
+function Buy({coin, portfolio, coins, authenticatedUser, orderType}) {
+  const [sellButton, setSellButton] = useState(      <button className="basis-1/2 border-2 text-xl bg-orange-400 text-orange-900 font-semibold border border-orange-500 rounded py-2 px-5 mx-auto active:border active:border-orange-400 hover:bg-orange-500">Sell</button>
+  );
+
+  // if (getCoinsInPortfolio().length === 0) {
+  //   sellButton = <button disabled onClick={() => setOrderType('sell')} className="basis-1/2 border-2 text-xl bg-zinc-800 text-orange-500 font-semibold border border-orange-500 rounded py-2 px-5 mx-auto active:border active:border-orange-400">Sell</button>
+  // } else {
+  //   sellButton = <button onClick={() => {
+  //     //if current selected coin is not in the user's portfolio, reset coin to first coin in portfolio
+  //     const coinsInPortfolio = getCoinsInPortfolio();
+  //     if (coinsInPortfolio.indexOf(coin.name) === -1) {
+  //       var coinToSet;
+  //       for (var i = 0; i < coins.length; i++) {
+  //         if (coins[i].name === coinsInPortfolio[0]) {
+  //           coinToSet = coins[i];
+  //           break;
+  //         }
+  //       }
+  //       setCoin(coinToSet);
+  //     }
+  //     setOrderType('sell')}
+  //   }
+  //     className="basis-1/2 border-2 text-xl bg-orange-400 text-orange-900 font-semibold border border-orange-500 rounded py-2 px-5 mx-auto active:border active:border-orange-400 hover:bg-orange-500">Sell</button>
+  // }
 
   return (
-    <div>Buy</div>
+    <div className="flex w-full h-16">
+      <button disabled={true} className="basis-1/2 border-2 text-xl bg-zinc-800 text-orange-500 font-semibold border border-orange-500 rounded py-2 px-5 mx-auto">Buy</button>
+      {sellButton}
+    </div>
   )
 }
 
