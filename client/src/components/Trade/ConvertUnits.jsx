9e0231870f409b102c0a1809f09c7e77b169765f
Morgan Harrison <morganharrison13@gmail.com>
2022-12-21 12:28:57 -0700

    Implemented state in convertUnits

--- client/src/components/Trade/ConvertUnits.jsx
+++ client/src/components/Trade/ConvertUnits.jsx
@@ -2,26 +2,24 @@ import React, { useState } from 'react';
 import { IconContext } from "react-icons";
 import { HiOutlineSwitchVertical } from 'react-icons/hi';
 
-function ConvertUnits({orderUnits, coin}) {
+function ConvertUnits({orderUnits, setOrderUnits, coin, setOrderAmount, total_trade_coin, total_trade_fiat}) {
   const conversionSymbol = orderUnits === 'usd' ? coin.acronym.toUpperCase() : '$';
 
-  // const handleUnitsConversion = () => {
-  //   if (orderUnits === 'usd') {
-  //     setOrderUnits('coin');
-  //     //take existing order amount and convert it to coin quantity
-  //     setOrderAmount(total_trade_coin);
-  //   } else if (orderUnits === 'coin') {
-  //     setOrderUnits('usd');
-  //     //take existing order amount and convert it to coin quantity
-  //     setOrderAmount(total_trade_fiat);
-  //   }
-  // }
-
-  // onClick={handleUnitsConversion}
+  const handleUnitsConversion = () => {
+    if (orderUnits === 'usd') {
+      setOrderUnits('coin');
+      //take existing order amount and convert it to coin quantity
+      setOrderAmount(total_trade_coin);
+    } else if (orderUnits === 'coin') {
+      setOrderUnits('usd');
+      //take existing order amount and convert it to coin quantity
+      setOrderAmount(total_trade_fiat);
+    }
+  }
 
   return (
     <div>
-      <button className="self-start hover:bg-zinc-600" >
+      <button className="self-start hover:bg-zinc-600"   onClick={handleUnitsConversion}>
         <IconContext.Provider value={{ size: "2em" }}>
           <HiOutlineSwitchVertical />
         </IconContext.Provider>
