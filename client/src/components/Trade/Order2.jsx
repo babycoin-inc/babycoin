df879a0423d270f43a9369f3e2f0057bbe001a45
Morgan Harrison <morganharrison13@gmail.com>
2022-12-21 12:19:53 -0700

    Implemented Buy order validation and styling

--- client/src/components/Trade/Order2.jsx
+++ client/src/components/Trade/Order2.jsx
@@ -9,21 +9,43 @@ import Price from './Price.jsx';
 function Order({ authenticatedUser, portfolio, coins, getPortfolioData, openModal, closeModal, populateModalValues, selectedCoin}) {
   const [orderType, setOrderType] = useState("buy");
   const [orderUnits, setOrderUnits] = useState('usd');
-  //DEFAULT COIN: CHANGE TO SELECTED COIN
+  const [orderAmount, setOrderAmount] = useState("");
   const [coin, setCoin] = useState(() => {
     if (selectedCoin !== undefined) {
       return selectedCoin;
     } else {
       return coins[1];
-    }});
- console.log('coins: ', coins);
+    }
+  });
+  let total_trade_coin;
+  var total_trade_fiat;
+  let isOrderValid = true;
+
+  console.log('coins: ', coins);
   console.log('portfolio: ', portfolio);
+  console.log('orderAmount1: ', orderAmount);
+
   const getCash = () => {
     const cashIndex = portfolio.findIndex((asset) => {
       return asset.acronym === 'usd'
     });
     return portfolio[cashIndex];
   };
+
+  const calculateSetTotals = (() => {
+    const value = parseFloat(orderAmount);
+    if (orderUnits === 'usd') {
+      total_trade_fiat = value;
+      total_trade_coin = value / coin.latest_price;
+    } else if (orderUnits === 'coin') {
+      //round down value at the fifth digit place
+      total_trade_fiat = value * coin.latest_price;
+      total_trade_coin = value;
+    }
+  })();
+
+  console.log('total_trade_coin: ', total_trade_coin);
+  console.log('total_trade_fiat: ', total_trade_fiat);
 
   const getNonCashAssets = () => {
     const nonCashAssets = [];
@@ -35,10 +57,30 @@ function Order({ authenticatedUser, portfolio, coins, getPortfolioData, openModa
     return nonCashAssets;
   }
 
-  // let [total_trade_coin, setTotalTradeCoin] = useState();
-  // let [total_trade_fiat, setTotalTradeFiat] = useState();
+  const getNonCashCoins = () => {
+    const nonCashCoins= [];
+    coins.forEach((coin) => {
+      if (coin.acronym !== 'usd') {
+        nonCashCoins.push(coin);
+      }
+    });
+    return nonCashCoins;
+  }
 
-  // buy props: coin={coin} portfolio={portflio} coins={coins} authenticatedUser={authenticatedUser} orderType={orderType}
+  const cash = getCash().quantity;
+  const maxCoinOrderAmount = cash / coin.latest_price;
+
+  if (orderType === 'buy' && orderUnits === 'usd'){
+    // if usd orderAmount exceeds available cash
+    if (total_trade_fiat > cash) {
+      isOrderValid = false;
+    }
+  } else if (orderType === 'buy' && orderUnits === 'coin'){
+    // if coin orderAmount exceeds available cash
+    if (total_trade_coin > maxCoinOrderAmount) {
+      isOrderValid = false;
+    }
+  }
 
   //ADD THE BELOW ONCLICK TO LAST DIV
   // onClick={submitOrder}
@@ -75,16 +117,18 @@ function Order({ authenticatedUser, portfolio, coins, getPortfolioData, openModa
 
   return (
     <div className="flex flex-col items-center space-y-8 w-1/3 bg-zinc-700 rounded-xl">
-      {orderType === 'buy' ? <Buy orderType={orderType} setOrderType={setOrderType} getNonCashAssets={getNonCashAssets} setCoin={setCoin} coin={coin} coins={coins}/> : <Sell Buy orderType={orderType} setOrderType={setOrderType}/>}
-      <OrderForm coin={coin} />
+      {orderType === 'buy' ? <Buy orderType={orderType} setOrderType={setOrderType} getNonCashAssets={getNonCashAssets} setCoin={setCoin} coin={coin} coins={coins} /> : <Sell Buy orderType={orderType} setOrderType={setOrderType} />}
+      <OrderForm coin={coin} orderUnits={orderUnits} setOrderUnits={setOrderUnits} orderType={orderType} total_trade_fiat={total_trade_fiat} total_trade_coin={total_trade_coin} getCash={getCash} orderAmount={orderAmount} setOrderAmount={setOrderAmount} isOrderValid={isOrderValid}/>
       {orderType === 'sell' ? <SellAll /> : undefined}
-      <TradeableCoins coins={orderType === 'buy' ? coins : coins} orderType={orderType} coin={coin} />
+      <TradeableCoins tradeableCoins={orderType === 'buy' ? getNonCashCoins() : getNonCashAssets()} orderType={orderType} coin={coin} setCoin={setCoin} coins={coins} />
       <Price coin={coin} />
       <div>
-        <button name="submit" className="text-lg mb-6 bg-orange-400 text-orange-900 font-semibold border border-orange-500 rounded-3xl py-2 px-5 mx-auto hover:bg-zinc-800 hover:border-zinc-800 hover:text-orange-500 active:border active:border-orange-400 h-14 w-44">Submit Order</button>
+        <button disabled={!isOrderValid} name="submit" className={`text-lg mb-6 font-semibold border border-orange-500 rounded-3xl py-2 px-5 mx-auto active:border active:border-orange-400 h-14 w-44 ${isOrderValid ? "hover:bg-zinc-800 hover:border-zinc-800 hover:text-orange-500 bg-orange-400 text-orange-900" : "bg-zinc-800 text-orange-500"}`} onClick={'do nothing'}>Submit Order</button>
       </div>
     </div>
   )
 }
 
 export default Order;
+
+// basis-1/2 border-2 text-xl bg-zinc-800 text-orange-500 font-semibold border border-orange-500 rounded py-2 px-5 mx-auto active:border active:border-orange-400
