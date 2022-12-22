569713c29e24414fe3d69892119dde411c1d4ae7
Nick Hays <nicholasahays@gmail.com>
2022-12-22 15:36:54 -0700

    Cleans up indents

--- client/src/components/Login/GoogleButton.jsx
+++ client/src/components/Login/GoogleButton.jsx
@@ -9,7 +9,12 @@ const GoogleButton = () => {
 
   return (
     <div className='flex justify-center py-8'>
-      <button className='border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center rounded-xl' onClick={handleGoogleLogin}> <FcGoogle className='mr-2' />Google</button>
+      <button
+        className='border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center rounded-xl'
+        onClick={handleGoogleLogin}>
+        <FcGoogle className='mr-2' />
+          Google
+      </button>
     </div>
   )
 }
