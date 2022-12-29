244dbd682ac587f026e4467a151776ddf612f9e0
David Chiu <42689526+dchiu91@users.noreply.github.com>
2022-12-29 14:44:56 -0800

    remove console.log

--- server/controllers/Leaderboard/leaderboard.js
+++ server/controllers/Leaderboard/leaderboard.js
@@ -2,7 +2,6 @@ const { Leaderboard } = require('../../../models/models.js');
 
 const getLeaderboard = async (req, res) => {
 	try {
-		console.log(req.query.search);
 		const result = await Leaderboard.getLeaderboard(req.query.duration,req.query.coin,req.query.page, req.query.search);
 		res.send(result);
 	}catch  (err) {
