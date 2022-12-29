05b0f8f51612d0e35cae4d3435ed942790518261
Morgan Harrison <morganharrison13@gmail.com>
2022-12-28 23:20:12 -0700

    Buy sell now allows a user to sell all of a coin w/o bugs

--- client/src/components/Trade/Order.jsx
+++ client/src/components/Trade/Order.jsx
@@ -10,7 +10,6 @@ function Order({ authenticatedUser, portfolio, coins, getPortfolioData, openAndP
   const [orderType, setOrderType] = useState("buy");
   const [orderUnits, setOrderUnits] = useState('usd');
   const [orderAmount, setOrderAmount] = useState("");
-  const [sellAll, setSellAll] = useState(false);
   const [coin, setCoin] = useState(() => {
     symbol = symbol.toUpperCase();
     if (symbol !== undefined) {
@@ -29,6 +28,11 @@ function Order({ authenticatedUser, portfolio, coins, getPortfolioData, openAndP
   let isOrderValid = true;
   let quantityOfCoin;
   let marketValueOfCoin;
+  let sellAll = false;
+
+  const setSellAll = (boolean) => {
+    sellAll = boolean;
+  }
 
   const getPortfolioQuantityOfCoin = () => {
     const index = portfolio.findIndex((asset, index) => { return asset.acronym === coin.acronym });
@@ -37,7 +41,7 @@ function Order({ authenticatedUser, portfolio, coins, getPortfolioData, openAndP
   };
 
   const getMarketValueInUSDOfCoin = () => {
-    marketValueOfCoin = quantityOfCoin * coin.latest_price;
+    marketValueOfCoin = getPortfolioQuantityOfCoin() * coin.latest_price;
     return marketValueOfCoin;
   }
 
@@ -54,6 +58,10 @@ function Order({ authenticatedUser, portfolio, coins, getPortfolioData, openAndP
     if (indexOfDec === -1) {
       return num;
     }
+    //if there's already the number of digits to the right of decimal
+    if (numAsString.slice(indexOfDec + 1).length <= digits) {
+      return num;
+    }
     let right = numAsString.slice(indexOfDec + 1, indexOfDec + 1 + digits);
     let left = numAsString.slice(-numAsString.length, indexOfDec + 1);
     return Number(left + right);
@@ -64,44 +72,39 @@ function Order({ authenticatedUser, portfolio, coins, getPortfolioData, openAndP
     setOrderAmount("");
   }
 
-  const sellAllOfCoin = () => {
-    //set total_trade_coin to max amount of quantityOfCoin
-    total_trade_coin = getPortfolioQuantityOfCoin();
-    total_trade_fiat = getMarketValueInUSDOfCoin();
-    //set total_trade_fiat to max amount of coin
-    console.log('total_trade_coin when sellAll: ', total_trade_coin);
-    console.log('total_trade_fiat when sellAll: ', total_trade_fiat);
-  }
-
   const calculateSetTotals = () => {
     const value = parseFloat(orderAmount);
     if (orderUnits === 'usd') {
       total_trade_fiat = value;
-      total_trade_coin = value / coin.latest_price;
-      //round total_trade_coin to the fifth decimal point based on total_trade_fiat / price
-      // total_trade_coin = roundNumUpToDigit(value / coin.latest_price, 5);
-      // total_trade_fiat = roundNumUpToDigit(total_trade_coin * coin.latest_price, 2);
-      console.log('total_trade_coin when orderUnits is usd: ', total_trade_coin);
+      total_trade_coin = roundNumUpToDigit(value / coin.latest_price, 7);
     } else if (orderUnits === 'coin') {
-      total_trade_fiat = value * coin.latest_price;
       total_trade_coin = value;
-      //round total_trade_fiat to the nearest cent based on total_trade_coin x price
-      // total_trade_fiat = roundNumUpToDigit(total_trade_coin * coin.latest_price, 2);
-      console.log('total_trade_coin when orderUnits is coin: ', total_trade_coin);
+      total_trade_fiat = roundNumUpToDigit(value * coin.latest_price, 2);
     }
   };
 
-  if (coin !== undefined) {
-    if (sellAll) {
-      sellAllOfCoin();
+  const isOrderAmountEqualToMaxSale = () => {
+    const value = parseFloat(orderAmount);
+    let amount;
+    if (orderUnits === 'usd') {
+      amount = roundNumUpToDigit(getMarketValueInUSDOfCoin(), 2);
+    }
+    if (orderUnits === 'coin') {
+      amount = roundNumUpToDigit(getPortfolioQuantityOfCoin(), 7);
+    }
+    if (value === amount) {
+      setSellAll(true);
     } else {
-      calculateSetTotals();
+      setSellAll(false);
     }
   }
 
-  console.log('orderAmount: ', orderAmount);
-  // console.log('total_trade_coin: ', total_trade_coin);
-  // console.log('total_trade_fiat: ', total_trade_fiat);
+  if (coin !== undefined) {
+    calculateSetTotals();
+    if (orderType === 'sell') {
+      isOrderAmountEqualToMaxSale();
+    }
+  }
 
   const getNonCashAssets = () => {
     const nonCashAssets = [];
@@ -121,6 +124,13 @@ function Order({ authenticatedUser, portfolio, coins, getPortfolioData, openAndP
       }
     });
     return nonCashCoins;
+  }
+
+  const sellAllOfCoin = () => {
+    //set total_trade_coin to max amount of quantityOfCoin
+    //set total_trade_fiat to max amount of coin
+    total_trade_coin = getPortfolioQuantityOfCoin();
+    total_trade_fiat = getMarketValueInUSDOfCoin();
   }
 
   const cash = getCash().quantity;
@@ -149,43 +159,41 @@ function Order({ authenticatedUser, portfolio, coins, getPortfolioData, openAndP
     getPortfolioQuantityOfCoin();
     if (orderUnits === 'usd') {
       if (total_trade_fiat > quantityOfCoin * coin.latest_price) {
-        // console.log('total_trade_fiat > quantityOfCoin * coin.latest_price', total_trade_fiat, ' > ', quantityOfCoin * coin.latest_price);
         isOrderValid = false
       }
     } else if (orderUnits === 'coin') {
       if (total_trade_coin > quantityOfCoin) {
-        // console.log('total_trade_coin > quantityOfCoin', total_trade_coin, ' > ',quantityOfCoin);
         isOrderValid = false;
       }
     }
   }
 
   const submitOrder = async () => {
+    let transaction = {
+      coin: coin,
+      orderType: orderType,
+      coin_id: coin.id,
+      currency: 'usd',
+      purchase_price: Number(coin.latest_price),
+      total_trade_coin: total_trade_coin,
+      total_trade_fiat: total_trade_fiat,
+      trader_id: authenticatedUser,
+      coinName: coin.name
+    }
     try {
-      const transaction = {
-        coin_id: coin.id,
-        currency: 'usd',
-        purchase_price: Number(coin.latest_price),
-        'total_trade_coin': total_trade_coin,
-        'total_trade_fiat': total_trade_fiat,
-        trader_id: authenticatedUser,
-        coinName: coin.name
-      }
-      //if sellAll is true
+      if (sellAll) {
         //send to a new endpoint where we handle the divisible by 0 error and remove the asset from portfolio
-      //otherwise...do the same as below
-      const orderResult = await axios.post(`/users/${authenticatedUser}/transactions/${orderType}`, transaction);
+        sellAllOfCoin();
+        transaction.total_trade_coin = total_trade_coin;
+        transaction.total_trade_fiat = total_trade_fiat;
+        const orderResult = await axios.post(`/users/${authenticatedUser}/transactions/sellAll`, transaction);
+      } else {
+        const orderResult = await axios.post(`/users/${authenticatedUser}/transactions/${orderType}`, transaction);
+      }
       await getPortfolioData(authenticatedUser);
-      openAndPopulateModal(
-        {
-          'coin': coin,
-          'total_trade_coin': total_trade_coin,
-          'total_trade_fiat': total_trade_fiat,
-          'purchase_price': Number(coin.latest_price),
-          'orderType': orderType
-        }
-      );
+      openAndPopulateModal(transaction);
       resetOrderForm();
+      setOrderType('buy');
       if (!achievementsStatus[2] && orderType === 'buy') {
         grantUserAchievement(2);
       }
@@ -195,16 +203,15 @@ function Order({ authenticatedUser, portfolio, coins, getPortfolioData, openAndP
       if (!achievementsStatus[4] && orderType === 'buy' && total_trade_fiat <= 5) {
         grantUserAchievement(4);
       }
-    }
-    catch (e) {
-      console.error(e);
-    }
-  };
+    } catch (e) {
+    console.error(e);
+  }
+};
 
   return (
     <div className="flex flex-col items-center space-y-8 bg-zinc-700 rounded-xl w-1/3 h-3/4">
       {orderType === 'buy' ? <Buy orderType={orderType} setOrderType={setOrderType} getNonCashAssets={getNonCashAssets} setCoin={setCoin} coin={coin} coins={coins} resetOrderForm={resetOrderForm} setOrderUnits={setOrderUnits}/> : <Sell Buy orderType={orderType} setOrderType={setOrderType} resetOrderForm={resetOrderForm} setOrderUnits={setOrderUnits} />}
-      <OrderForm coin={coin} orderUnits={orderUnits} setOrderUnits={setOrderUnits} orderType={orderType} total_trade_fiat={total_trade_fiat} total_trade_coin={total_trade_coin} getCash={getCash} orderAmount={orderAmount} setOrderAmount={setOrderAmount} isOrderValid={isOrderValid} quantityOfCoin={quantityOfCoin} roundNumUpToDigit={roundNumUpToDigit} setSellAll={setSellAll}/>
+      <OrderForm coin={coin} orderUnits={orderUnits} setOrderUnits={setOrderUnits} orderType={orderType} total_trade_fiat={total_trade_fiat} total_trade_coin={total_trade_coin} getCash={getCash} orderAmount={orderAmount} setOrderAmount={setOrderAmount} isOrderValid={isOrderValid} quantityOfCoin={quantityOfCoin} roundNumUpToDigit={roundNumUpToDigit} setSellAll={setSellAll} sellAll={sellAll}/>
       <TradeableCoins tradeableCoins={orderType === 'buy' ? getNonCashCoins() : getNonCashAssets()} resetOrderForm={resetOrderForm} orderType={orderType} coin={coin} setCoin={setCoin} coins={coins} />
       <Price coin={coin} />
       <div>
