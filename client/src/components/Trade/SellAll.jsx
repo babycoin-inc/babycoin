7a9c2df06f25b4ead9cfc305c68e947c0a41c0f7
Morgan Harrison <morganharrison13@gmail.com>
2022-12-28 21:03:06 -0700

    Disabled sellAll button when enabled

--- client/src/components/Trade/SellAll.jsx
+++ client/src/components/Trade/SellAll.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 function SellAll({coin, setOrderAmount, quantityOfCoin, orderUnits, roundNumUpToDigit, setSellAll, sellAll}) {
 
   return (
-        <button name="sellAll" className={`text-center bg-zinc-400 rounded-xl hover:bg-zinc-500 py-2 px-10 mx-auto relative left-5 ${sellAll ? 'border border-2 border-green-500 py-2 px-10 mx-auto' : undefined}`} onClick={() => {
+        <button disabled={sellAll} name="sellAll" className={`text-center bg-zinc-400 rounded-xl hover:bg-zinc-500 py-2 px-10 mx-auto relative left-5 ${sellAll ? 'border border-2 border-green-500 py-2 px-10 mx-auto' : undefined}`} onClick={() => {
           const amount = orderUnits === 'usd' ? roundNumUpToDigit(quantityOfCoin * coin.latest_price, 2) : roundNumUpToDigit(quantityOfCoin, 7);
           setOrderAmount(amount);
           setSellAll(true);
