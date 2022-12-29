347aa6a188372c8122ca5168c46a072194368478
Nick Hays <nicholasahays@gmail.com>
2022-12-28 17:03:18 -0700

    Adds lighter orange color on hover of form switch text

--- client/src/components/Login/Auth.jsx
+++ client/src/components/Login/Auth.jsx
@@ -18,10 +18,11 @@ const Auth = ({ updateUser }) => {
       <div className='flex flex-col justify-center items-center pt-28'>
         <img className= 'w-48 h-48' src={babycoinLogo} alt='babycoin'/>
       </div>
-      <div className='max-w-[400px] w-full mx-auto p-20 bg-zinc-800 rounded-xl -mt-14 text-zinc-200'>
+      {/* <div className='max-w-[400px] w-full mx-auto p-20 bg-gradient-to-br from-zinc-600 via-zinc-700 to-zinc-800 rounded-xl -mt-14 text-zinc-200'> */}
+      <div className='max-w-[400px] w-full mx-auto p-20 bg-metallic rounded-xl -mt-14 text-zinc-200'>
         <h2 className='text-6xl font-bold text-center py-4 italic text-zinc-200'>babycoin</h2>
         <div className='text-center hover:scale-105'>
-          <small>{isLoginForm ? 'Not' : 'Already'} a member? <a className='text-orange-700 cursor-pointer' onClick={handleWhichForm}>{isLoginForm ? 'Sign Up Now!' : 'Login Now!'}</a></small>
+          <small>{isLoginForm ? 'Not' : 'Already'} a member? <a className='text-orange-700 hover:text-orange-500 cursor-pointer' onClick={handleWhichForm}>{isLoginForm ? 'Sign Up Now!' : 'Login Now!'}</a></small>
         </div>
         <GoogleButton />
         {isLoginForm ? <LoginForm updateUser={updateUser} /> : <SignupForm updateUser={updateUser}/>}
