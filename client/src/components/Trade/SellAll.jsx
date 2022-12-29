ae45b32a9b8fe91caa6744e487bb968f80e5a57b
Morgan Harrison <morganharrison13@gmail.com>
2022-12-28 20:58:11 -0700

    Add styling to sellAll button when activated

--- client/src/components/Trade/SellAll.jsx
+++ client/src/components/Trade/SellAll.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
 
-function SellAll({coin, setOrderAmount, quantityOfCoin, orderUnits, roundNumUpToDigit, setSellAll}) {
+function SellAll({coin, setOrderAmount, quantityOfCoin, orderUnits, roundNumUpToDigit, setSellAll, sellAll}) {
 
   return (
-    <div className="text-center bg-zinc-400 rounded-xl hover:bg-zinc-500 py-2 px-10 mx-auto relative left-5">
-        <button onClick={() => {
-          const amount = orderUnits === 'usd' ? roundNumUpToDigit(quantityOfCoin * coin.latest_price, 2) : roundNumUpToDigit(quantityOfCoin, 5);
+        <button name="sellAll" className={`text-center bg-zinc-400 rounded-xl hover:bg-zinc-500 py-2 px-10 mx-auto relative left-5 ${sellAll ? 'border border-2 border-green-500 py-2 px-10 mx-auto' : undefined}`} onClick={() => {
+          const amount = orderUnits === 'usd' ? roundNumUpToDigit(quantityOfCoin * coin.latest_price, 2) : roundNumUpToDigit(quantityOfCoin, 7);
           setOrderAmount(amount);
           setSellAll(true);
         }
         }>Sell All</button>
-      </div>
   )
 }
 
 export default SellAll;
+
+{/* <div className="text-center bg-zinc-400 rounded-xl hover:bg-zinc-500 py-2 px-10 mx-auto relative left-5">
+<button name="sellAll" className={`${sellAll ? 'border border-2 border-green-500 py-2 px-5 mx-auto' : undefined}`} onClick={() => {
+  const amount = orderUnits === 'usd' ? roundNumUpToDigit(quantityOfCoin * coin.latest_price, 2) : roundNumUpToDigit(quantityOfCoin, 7);
+  setOrderAmount(amount);
+  setSellAll(true);
+}
+}>Sell All</button>
+</div> */}
