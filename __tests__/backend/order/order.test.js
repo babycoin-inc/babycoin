cd60309b6c02fd8f8239b305048f10a07416afac
Morgan Harrison <morganharrison13@gmail.com>
2022-12-30 12:55:49 -0700

    Added skeleton of backend test cases for trade

--- __tests__/backend/order/order.test.js
+++ __tests__/backend/order/order.test.js
@@ -7,3 +7,13 @@ describe('dummy test', () => {
     expect(sum(2, 4)).toBe(6)
   })
 })
+
+/* Transaction TEST CASES
+Use all USD to buy one coin
+Sell all of one coin
+
+Buy coin1 w/ portion of cash
+Buy coin1 w/ portion of cash
+Buy coin2 w/ portion of cash
+Sell partial of coin1
+*/
