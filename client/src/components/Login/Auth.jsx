5108bc9cc96f53c62ebe6af88eb34211bfff842d
Nick Hays <nicholasahays@gmail.com>
2022-12-30 11:45:58 -0700

    Changes padding of info card so that box height remains consistent

--- client/src/components/Login/Auth.jsx
+++ client/src/components/Login/Auth.jsx
@@ -30,7 +30,7 @@ const Auth = ({ updateUser }) => {
       </div>
       <div className='max-w-[400px] w-full mx-auto bg-bitcoin-pattern rounded-xl -mt-14 text-zinc-200 border-4 border-zinc-800'>
         {infoIsShown ?
-          <div id='infoContainer' className='px-9 py-20'>
+          <div id='infoContainer' className='px-9 py-[79px]'> {/* 79px keeps box from changing height between switching 'cards'*/}
            Babycoin is a cryptocurrency trading platform designed for aspiring traders who want to learn the ins and outs of trading without risking real money. The platform allows users to trade with practice currency in a sandbox environment, giving them the opportunity to try out different strategies and techniques without the pressure of actual financial risk. Babycoin also offers a range of achievements and lessons designed to teach basic trading skills and help users become more confident and knowledgeable traders. Whether you are just starting out in the world of cryptocurrency trading or are looking to improve your skills, Babycoin is a great resource for learning and practicing your trades.
           </div>
         :
@@ -47,7 +47,7 @@ const Auth = ({ updateUser }) => {
       </div>
       <div id='info' className='mt-6 text-center text-zinc-200'>
         <button onClick={toggleInfo}>
-          ..what is babycoin?
+          {infoIsShown ? <em>back to login..</em> : <em>..what is babycoin?</em> }
         </button>
       </div>
     </div>
