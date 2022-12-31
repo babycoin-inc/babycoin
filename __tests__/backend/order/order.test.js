10aed65868f07fccdc0dd6f3edd36af95ed5ea4f
Morgan Harrison <morganharrison13@gmail.com>
2022-12-31 00:00:25 -0700

    Added framework for backend tests

--- __tests__/backend/order/order.test.js
+++ __tests__/backend/order/order.test.js
@@ -1,12 +1,25 @@
-const sum = (a, b) => {
-  return a + b
-}
+require("dotenv").config();
+const { query } = require('../../../db/index.js');
+const { Trade } = require('../../../models/models.js');
+
+// const trader_id = 1;
+// const transaction = {
+//   coin_id: coin.id,
+//   currency: 'usd',
+//   purchase_price: Number(coin.latest_price),
+//   total_trade_coin: total_trade_coin,
+//   total_trade_fiat: total_trade_fiat,
+//   trader_id: authenticatedUser,
+// }
 
 describe('dummy test', () => {
   test('adds 2 + 4 to equal 6', () => {
+
     expect(sum(2, 4)).toBe(6)
   })
 })
+
+//try querying transaction table
 
 /* Transaction TEST CASES
 Use all USD to buy one coin
