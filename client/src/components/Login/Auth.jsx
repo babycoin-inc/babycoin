8d1c6b79a9f111c493d210de037ab4eea3a5b7a5
Nick Hays <nicholasahays@gmail.com>
2022-12-30 20:19:02 -0700

    Experimented with absolute positioning for login/info box

--- client/src/components/Login/Auth.jsx
+++ client/src/components/Login/Auth.jsx
@@ -20,6 +20,8 @@ const Auth = ({ updateUser }) => {
     setInfoIsShown(!infoIsShown);
   }
 
+  const
+
   return (
     <div className={`relative w-full min-h-screen`}>
       {/* Logo container */}
@@ -34,12 +36,15 @@ const Auth = ({ updateUser }) => {
       <div className='bg-zinc-800 border-2 border-zinc-600 max-w-[450px] w-full -mt-14 mx-auto rounded-xl pb-10'> {/** h-[680px]*/}
 
         {/* solid background card */}
-        <div className='border-2 border-orange-700 max-w-[375px] bg-bitcoin-pattern rounded-xl mx-[37.5px] mt-10  text-zinc-200'> {/* -mt-14 */}
+        <div className='group'>
+        <div className='border-2 border-zinc-900 max-w-[375px] bg-bitcoin-pattern rounded-xl mx-[37.5px] mt-10 text-zinc-200 transition-all duration-700 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]'> {/* -mt-14 */}
 
           {/* INFO BOX */}
           {infoIsShown ?
-            <div id='infoContainer' className='px-8 py-[71px] text-center' > {/* className='px-9 py-[79px]' 79px keeps box from changing height between switching 'cards'*/}
-            Babycoin is a cryptocurrency trading platform designed for aspiring traders who want to learn the ins and outs of trading without risking real money. The platform allows users to trade with practice currency in a sandbox environment, giving them the opportunity to try out different strategies and techniques without the pressure of actual financial risk. Babycoin also offers a range of achievements and lessons designed to teach basic trading skills and help users become more confident and knowledgeable traders. Whether you are just starting out in the world of cryptocurrency trading or are looking to improve your skills, Babycoin is a great resource for learning and practicing your trades.
+            <div id='infoContainer' className='bg-bitcoin-pattern [transform:rotateY(180deg)] [backface-visibility:hidden]' > {/* className='px-9 py-[79px]' 79px keeps box from changing height between switching 'cards'*/}
+              <div className='px-8 py-[71px] text-center'>
+                Babycoin is a cryptocurrency trading platform designed for aspiring traders who want to learn the ins and outs of trading without risking real money. The platform allows users to trade with practice currency in a sandbox environment, giving them the opportunity to try out different strategies and techniques without the pressure of actual financial risk. Babycoin also offers a range of achievements and lessons designed to teach basic trading skills and help users become more confident and knowledgeable traders. Whether you are just starting out in the world of cryptocurrency trading or are looking to improve your skills, Babycoin is a great resource for learning and practicing your trades.
+              </div>
             </div>
           :
           // LOGIN SIGNUP BOX
@@ -54,6 +59,7 @@ const Auth = ({ updateUser }) => {
               {isLoginForm ? <LoginForm updateUser={updateUser} /> : <SignupForm updateUser={updateUser} />}
             </div>
           }
+        </div>
         </div>
       </div>
 
