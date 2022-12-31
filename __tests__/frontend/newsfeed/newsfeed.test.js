22198b8cb15726f66276a6f9040df92c579bb4ee
Alex Yeung <alexyeung@Rogelios-MacBook-Pro.local>
2022-12-31 20:45:48 +0900

    Updated

--- __tests__/frontend/newsfeed/newsfeed.test.js
+++ __tests__/frontend/newsfeed/newsfeed.test.js
@@ -1,11 +1,4 @@
 import "../client/src/components/Newsfeeds/newsfeed.jsx"
-
-const sum = (a, b) => {
-  return a + b
-}
-
-describe('dummy test', () => {
-  test('adds 2 + 4 to equal 6', () => {
-    expect(sum(2, 4)).toBe(6)
-  })
-})
+import {render, fireEvent, screen} from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import '@testing-library/jest-dom';
