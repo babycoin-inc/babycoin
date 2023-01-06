55e2f7e781fe676892170c48ecc65535df34c97e
Nick Hays <nicholasahays@gmail.com>
2023-01-05 18:51:41 -0700

    Adds basic render check to make sure login component is rendering correctly (still having trouble implement submit check)

--- __tests__/frontend/auth/auth.test.js
+++ __tests__/frontend/auth/auth.test.js
@@ -1,9 +1,41 @@
-const sum = (a, b) => {
-  return a + b
-}
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import LoginForm from '../../../client/src/components/Login/LoginForm.jsx';
 
-describe('dummy test', () => {
-  test('adds 2 + 4 to equal 6', () => {
-    expect(sum(2, 4)).toBe(6)
-  })
-})
+afterEach(cleanup);
+
+// test('Login form submits correctly', () => {
+//   const onSubmit = jest.fn();
+//   const { getByLabelText, getByText } = render(<LoginForm onSubmit={onSubmit}/>)
+
+//   //simulate filling out the form
+//   const usernameInput = getByLabelText('Username');
+//   fireEvent.change(usernameInput, { target: { value: 'testuser' } });
+//   const passwordInput = getByLabelText('Password');
+//   fireEvent.change(passwordInput, { target: { value: 'password' } });
+
+//   //simulate submitting the form
+//   const submitButton = getByText('Log In');
+//   fireEvent.click(submitButton);
+
+//   //verifies the mock function (onSubmit) was called with the correct arguments
+//   expect(onSubmit).toHaveBeenCalledWith({
+//     username: 'testuser',
+//     password: 'password'
+//   });
+
+// });
+
+test('Login form is rendered correctly', () => {
+  const { getByLabelText, getByText } = render(<LoginForm />);
+
+  // check for the presence of the form inputs
+  expect(getByLabelText('Username')).toBeInTheDocument();
+  expect(getByLabelText('Password')).toBeInTheDocument();
+
+  // check for the presence of the submit button
+  expect(getByText('Log In')).toBeInTheDocument();
+});
+
+
