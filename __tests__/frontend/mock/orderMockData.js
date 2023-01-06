d0dc25c75b054cc9f04f44ccf8e7dd513acdc21b
Morgan Harrison <morganharrison13@gmail.com>
2023-01-05 22:35:55 -0700

    Created mock data for order component

---
+++ __tests__/frontend/mock/orderMockData.js
@@ -0,0 +1,236 @@
+module.exports = {
+  portfolioMockData: [
+    {
+        "coin": "Bitcoin",
+        "acronym": "btc",
+        "value": 111.3074556537,
+        "quantity": 0.00666667,
+        "avg_entry": 15000,
+        "curr_price": 16696.11,
+        "profit_loss": 11.3074556537,
+        "dollar_cost": 100,
+        "percent_change": 11.3074556537,
+        "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+    },
+    {
+        "coin": "Solana",
+        "acronym": "sol",
+        "value": 0.1119,
+        "quantity": 0.01,
+        "avg_entry": 10000,
+        "curr_price": 11.19,
+        "profit_loss": -99.8881,
+        "dollar_cost": 100,
+        "percent_change": -99.8881,
+        "image": "https://assets.coingecko.com/coins/images/4128/large/solana.png?1640133422"
+    },
+    {
+        "coin": "Ethereum",
+        "acronym": "eth",
+        "value": 8.1045373856,
+        "quantity": 0.00666667,
+        "avg_entry": 15000,
+        "curr_price": 1215.68,
+        "profit_loss": -91.8954626144,
+        "dollar_cost": 100,
+        "percent_change": -91.8954626144,
+        "image": "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880"
+    },
+    {
+        "coin": "Cardano",
+        "acronym": "ada",
+        "value": 0.00253439,
+        "quantity": 0.01,
+        "avg_entry": 10000,
+        "curr_price": 0.253439,
+        "profit_loss": -99.99746561,
+        "dollar_cost": 100,
+        "percent_change": -99.99746561,
+        "image": "https://assets.coingecko.com/coins/images/975/large/cardano.png?1547034860"
+    },
+    {
+        "coin": "Dogecoin",
+        "acronym": "doge",
+        "value": 0.00047758690546,
+        "quantity": 0.00666667,
+        "avg_entry": 15000,
+        "curr_price": 0.071638,
+        "profit_loss": -99.99952241309454,
+        "dollar_cost": 100,
+        "percent_change": -99.99952241309454,
+        "image": "https://assets.coingecko.com/coins/images/5/large/dogecoin.png?1547792256"
+    },
+    {
+        "coin": "BNB",
+        "acronym": "bnb",
+        "value": 2.4586,
+        "quantity": 0.01,
+        "avg_entry": 10000,
+        "curr_price": 245.86,
+        "profit_loss": -97.5414,
+        "dollar_cost": 100,
+        "percent_change": -97.5414,
+        "image": "https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1644979850"
+    },
+    {
+        "coin": "Polygon",
+        "acronym": "matic",
+        "value": 0.00520503593585,
+        "quantity": 0.00666667,
+        "avg_entry": 15000,
+        "curr_price": 0.780755,
+        "profit_loss": -99.99479496406416,
+        "dollar_cost": 100,
+        "percent_change": -99.99479496406416,
+        "image": "https://assets.coingecko.com/coins/images/4713/large/matic-token-icon.png?1624446912"
+    },
+    {
+        "coin": "XRP",
+        "acronym": "xrp",
+        "value": 0.00343483,
+        "quantity": 0.01,
+        "avg_entry": 10000,
+        "curr_price": 0.343483,
+        "profit_loss": -99.99656517,
+        "dollar_cost": 100,
+        "percent_change": -99.99656517,
+        "image": "https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1605778731"
+    },
+    {
+        "coin": "Tether",
+        "acronym": "usdt",
+        "value": 0.01,
+        "quantity": 0.01,
+        "avg_entry": 10000,
+        "curr_price": 1,
+        "profit_loss": -99.99,
+        "dollar_cost": 100,
+        "percent_change": -99.99,
+        "image": "https://assets.coingecko.com/coins/images/325/large/Tether.png?1668148663"
+    },
+    {
+      "coin": "US Dollar",
+      "acronym": "usd",
+      "value": 500,
+      "quantity": 500,
+      "avg_entry": 1,
+      "curr_price": 1,
+      "profit_loss": 0,
+      "dollar_cost": 1,
+      "percent_change": 0
+    }
+  ],
+  coins:
+  [
+    {
+      id: 1,
+      name: 'United States Dollar',
+      acronym: 'usd',
+      image: 'https://res.cloudinary.com/dc3r923zh/image/upload/v1671498243/boc/DALL_E-usd_eaaefm.png',
+      description: null,
+      latest_price: '1',
+      price_change_percentage: '0',
+      market_cap: null
+    },
+    {
+      id: 5,
+      name: 'Tether',
+      acronym: 'usdt',
+      image: 'https://assets.coingecko.com/coins/images/325/large/Tether.png?1668148663',
+      description: null,
+      latest_price: '1',
+      price_change_percentage: '0.00568',
+      market_cap: '66278648652'
+    },
+    {
+      id: 9,
+      name: 'Ethereum',
+      acronym: 'eth',
+      image: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880',
+      description: null,
+      latest_price: '1251.21',
+      price_change_percentage: '-0.41631',
+      market_cap: '150805231900'
+    },
+    {
+      id: 7,
+      name: 'Dogecoin',
+      acronym: 'doge',
+      image: 'https://assets.coingecko.com/coins/images/5/large/dogecoin.png?1547792256',
+      description: null,
+      latest_price: '0.071557',
+      price_change_percentage: '-2.31859',
+      market_cap: '9842815302'
+    },
+    {
+      id: 6,
+      name: 'Cardano',
+      acronym: 'ada',
+      image: 'https://assets.coingecko.com/coins/images/975/large/cardano.png?1547034860',
+      description: null,
+      latest_price: '0.269411',
+      price_change_percentage: '0.54587',
+      market_cap: '9439944979'
+    },
+    {
+      id: 11,
+      name: 'XRP',
+      acronym: 'xrp',
+      image: 'https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1605778731',
+      description: null,
+      latest_price: '0.338511',
+      price_change_percentage: '-2.7882',
+      market_cap: '17116070602'
+    },
+    {
+      id: 2,
+      name: 'Bitcoin',
+      acronym: 'btc',
+      image: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+      description: null,
+      latest_price: '16835.14',
+      price_change_percentage: '-0.15486',
+      market_cap: '324138957221'
+    },
+    {
+      id: 10,
+      name: 'Polkadot',
+      acronym: 'dot',
+      image: 'https://assets.coingecko.com/coins/images/12171/large/polkadot.png?1639712644',
+      description: null,
+      latest_price: '4.62',
+      price_change_percentage: '-0.25574',
+      market_cap: '5486559229'
+    },
+    {
+      id: 8,
+      name: 'Polygon',
+      acronym: 'matic',
+      image: 'https://assets.coingecko.com/coins/images/4713/large/matic-token-icon.png?1624446912',
+      description: null,
+      latest_price: '0.789253',
+      price_change_percentage: '-2.1745',
+      market_cap: '7074065160'
+    },
+    {
+      id: 3,
+      name: 'Solana',
+      acronym: 'sol',
+      image: 'https://assets.coingecko.com/coins/images/4128/large/solana.png?1640133422',
+      description: null,
+      latest_price: '13.44',
+      price_change_percentage: '-0.14428',
+      market_cap: '4943576397'
+    },
+    {
+      id: 4,
+      name: 'BNB',
+      acronym: 'bnb',
+      image: 'https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1644979850',
+      description: null,
+      latest_price: '256.54',
+      price_change_percentage: '-1.00054',
+      market_cap: '41882966205'
+    }
+  ]
+}
