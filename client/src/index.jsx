81e10aa57d9e2361ce3d3f203e8206f7a6635df8
Nick Hays <nicholasahays@gmail.com>
2023-01-21 19:10:16 -0700

    Removes type of root

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import '../dist/styles/tailwind.css'
 import Page from './components/Page.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Page />
